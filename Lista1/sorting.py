723f6735b81638455ccdff2c02f7e334b3127c7b
gorbart <bartek.go3@gmail.com>
2021-03-01 20:26:10 +0100

    mergesort done and tested; added assertions

--- Lista1/sorting.py
+++ Lista1/sorting.py
@@ -1,6 +1,3 @@
-import random
-from timeit import default_timer as timer
-
 
 def quicksort(num_list):
     if len(num_list) <= 1:
@@ -13,9 +10,53 @@ def quicksort(num_list):
     return quicksort(lesser) + equal + quicksort(greater)
 
 
+def mergesort(num_list):
+    list_len = len(num_list)
 
-l = [2, 5, 1, 8, 12, -5, 4, 20, 0]
-start = timer()
-quicksort(l)
-end = timer()
-print(end - start)
+    if list_len == 0:
+        return
+    elif list_len == 1:
+        return num_list
+
+    mid = list_len // 2
+    left_list = num_list[:mid]
+    right_list = num_list[mid:]
+
+    mergesort(left_list)
+    mergesort(right_list)
+
+    left_iter = 0
+    right_iter = 0
+    main_iter = 0
+
+    left_length = len(left_list)
+    right_length = len(right_list)
+
+    while left_iter < left_length and right_iter < right_length:
+        if left_list[left_iter] < right_list[right_iter]:
+            num_list[main_iter] = left_list[left_iter]
+            left_iter += 1
+        else:
+            num_list[main_iter] = right_list[right_iter]
+            right_iter += 1
+
+        main_iter += 1
+
+    while left_iter < left_length:
+        num_list[main_iter] = left_list[left_iter]
+        left_iter += 1
+        main_iter += 1
+
+    while right_iter < right_length:
+        num_list[main_iter] = right_list[right_iter]
+        right_iter += 1
+        main_iter += 1
+
+    return num_list
+
+
+test_list = [2, 5, 1, 8, 12, -5, 4, 20, 0, 500, -54, 25, 12, -544]
+control_list = [-544, -54, -5, 0, 1, 2, 4, 5, 8, 12, 12, 20, 25, 500]
+
+assert control_list == quicksort(test_list)
+assert control_list == mergesort(test_list)
