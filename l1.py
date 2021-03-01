39a812b28fd404ebb1db3778c12baae5a6bd7ec3
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-03-01 11:52:46 +0100

    Added quicksort

--- l1.py
+++ l1.py
@@ -1,16 +1,45 @@
-numbers = [10, 5, 2, 11, 200, -50, 10, 0, 1, -5]
-
 def swap(array, firstIndex, secondIndex):
     tmp = array[firstIndex]
     array[firstIndex] = array[secondIndex]
     array[secondIndex] = tmp
 
-def bubbleSort(array):
+def bubblesort(array):
     for i in range(len(array)):
         for j in range(0, len(array) - i - 1):
             if array[j] > array [j + 1]:
                 swap(array, j, j + 1)
-    
 
-bubbleSort(numbers)
-print(numbers)
+def quicksort(array, start, end):
+    if start >= end:
+        return
+
+    left = start
+    right = end
+    pivot = array[start + (end - start) / 2]
+
+    while (left <= right):
+        while (array[left] < pivot):
+            left = left + 1
+        
+        while (array[right] > pivot):
+            right = right - 1;
+        
+        if (left <= right):
+            swap(array, left, right);
+            left = left + 1;
+            right = right - 1;
+
+    if(start < right):
+        quicksort(array, start, right)
+    if(left < end):
+        quicksort(array, left, end)    
+
+# Test
+numbersBubble = [10, 5, 2, 11, 200, -50, 10, 0, 1, -5]
+numbersQuick = [10, 5, 2, 11, 200, -50, 10, 0, 1, -5]
+
+quicksort(numbersBubble, 0, len(numbersBubble) - 1)
+quicksort(numbersQuick, 0, len(numbersQuick) - 1)
+
+print(numbersBubble)
+print(numbersQuick)
