c0bc31d95724b351c4d739177bbdb984214bd131
Jakub Kochanski <jakub1134@o2.pl>
2021-03-03 18:50:04 +0100

    QuickSort implementation with tests

--- 1/venv/Include/Main.py
+++ 1/venv/Include/Main.py
@@ -8,6 +8,29 @@ def selection_sort(array):
         swap(array, smallest_value_index + i, i)
 
 
+def quick_sort(array):
+    sort(array, 0, len(array) - 1)
+
+
+def sort(array, start, end):
+    if start < end:
+        split = quick(array, start, end)
+        sort(array, start, split - 1)
+        sort(array, split + 1, end)
+
+
+def quick(array, start, end):
+    current_index = start
+    pivot = array[(start + end) // 2]
+    swap(array, (start + end) // 2, end)
+    for i in range(start, end):
+        if array[i] < pivot:
+            swap(array, i, current_index)
+            current_index = current_index + 1
+    swap(array, end, current_index)
+    return current_index
+
+
 def swap(array, current_index, new_index):
     tmp = array[current_index]
     array[current_index] = array[new_index]
@@ -19,7 +42,8 @@ def sorting_test(sorting_method):
     print("Test 1 - small random int: {}".format(sorting_methods_test(sorting_method, random_int_array(100))))
     print("Test 2 - big random int: {}".format(sorting_methods_test(sorting_method, random_int_array(10000))))
     print("Test 3 - ordered int: {}".format(sorting_methods_test(sorting_method, ordered_int_array(1000, True))))
-    print("Test 4 - reverse ordered int: {}".format(sorting_methods_test(sorting_method, ordered_int_array(1000, False))))
+    print(
+        "Test 4 - reverse ordered int: {}".format(sorting_methods_test(sorting_method, ordered_int_array(1000, False))))
     print("Test 5 - small random string: {}".format(sorting_methods_test(sorting_method, random_string_array(10))))
     print("Test 6 - big random string: {}".format(sorting_methods_test(sorting_method, random_string_array(1000))))
 
@@ -44,3 +68,4 @@ def random_string_array(size):
 
 if __name__ == '__main__':
     sorting_test(selection_sort)
+    sorting_test(quick_sort)
