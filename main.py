baa9af8dc7432da82a99a9f6d2e4caa9925c39b3
Reknor <33392647+Reknor@users.noreply.github.com>
2021-03-03 17:38:00 +0100

    Added InsertSort

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ def bubble_sort(array):
                 array[j] = temp
 
 
+def insert_sort(array):
+    for i in range(1, len(array)):
+        current = array[i]
+        j = i-1
+        while j >= 0 and current < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = current
+
+
 array = [1, 3, 0, 2, 5, 7, 9]
+array2 = [1, 3, 0, 2, 5, 7, 9]
 bubble_sort(array)
+insert_sort(array2)
 print(array)
+print(array2)
 
