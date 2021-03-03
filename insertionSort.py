1917894043205307452f0874b595ef87ce312921
Witek <hello>
2021-03-03 18:10:24 +0100

    Insertion sort algorithm added

---
+++ insertionSort.py
@@ -0,0 +1,14 @@
+def insertionSort(arr):
+    
+    size = len(arr)
+    
+    for i in range(1, size):
+        tmp = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > tmp :
+            arr[j + 1] = arr[j]
+            j = j - 1
+
+        arr[j + 1] = tmp
+
+
