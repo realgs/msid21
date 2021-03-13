2df87efdc247e63163d269c3764a283c1aa65dff
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2021-03-13 12:34:09 +0100

    add main.py

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -1,6 +1,3 @@
-import random
-
-
 def bubble_sort(number_array):
     size = len(number_array)
     for i in range(1, size):
@@ -9,15 +6,3 @@ def bubble_sort(number_array):
             if number_array[left] >= number_array[right]:
                 number_array[left], number_array[right] = number_array[right], number_array[left]
     return number_array
-
-
-def create_array(size, start, end):
-    number_array = []
-    for i in range(size):
-        number_array.append(random.randint(start, end))
-    return number_array
-
-
-array_1 = create_array(10, 1, 500)
-print("Created array: ", array_1)
-print("Sorted array: ", bubble_sort(array_1))
--- Sorting/insertsort.py
+++ Sorting/insertsort.py
@@ -1,6 +1,3 @@
-import random
-
-
 def insert_sort(number_array):
     size = len(number_array)
     for i in range(1, size):
@@ -11,15 +8,3 @@ def insert_sort(number_array):
             j -= 1
         number_array[j + 1] = value
     return number_array
-
-
-def create_array(size, start, end):
-    number_array = []
-    for i in range(size):
-        number_array.append(random.randint(start, end))
-    return number_array
-
-
-array_1 = create_array(10, 1, 500)
-print("Created array: ", array_1)
-print("Sorted array: ", insert_sort(array_1))
