c30b9dc19d726bb2136d3ec6b9f8ac098a524d35
Mateusz_Mandziuk <254303@student.pwr.edu.pl>
2021-03-13 14:34:22 +0100

    Impelmentation of first sorting method- bubble sort

--- SortMet.py
+++ SortMet.py
@@ -1,1 +1,15 @@
 # 2 sorting methods: bubble sort and quicksort
+
+example = [20, 17, 4, 2, 11, 14, 21, 18, 7, 1, 10, 13]
+
+def bubble_sort(list):
+    l = len(list)
+    for i in range(l-1):
+        for j in range(0, l -i-1 ):
+            if list[j]>list[j+1]:
+                temp = list[j]
+                list[j]= list[j+1]
+                list[j+1] = temp
+    return list
+
+print(bubble_sort(example))
