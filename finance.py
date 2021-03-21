a40bd7351d3072e35efeaae806dab3cdcd8cf8b4
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-03-21 23:26:21 +0100

    Calculating diff once

--- finance.py
+++ finance.py
@@ -2,20 +2,25 @@ import requests
 from datetime import datetime
 
 
-def _getLastTransactions(good, limit=50):
+def _getApiResponse(good, limit=50):
     url = "https://bitbay.net/API/Public/" + good + "/trades.json?sort=desc&limit=" + str(limit)
 
     headers = {'content-type': 'application/json'}
     response = requests.get(url, headers=headers)
-
-    print()
     if response.status_code == 200:
-        print("Last " + str(limit) + " transactions for " + good + ":")
-        for tran in response.json():
-            print("Tid: ", tran['tid'], " -  Type: ", tran['type'], " -  Price: ", tran['price'], " -  Amount: ",
-                  tran['amount'], " -  Date: ", datetime.fromtimestamp(tran['date']))
+        return response.json()
     else:
         print(response.reason)
+        return []
+
+
+def _getLastTransactions(good, limit=50):
+    transactions = _getApiResponse(good, limit)
+    if transactions:
+        print("\n", "Last " + str(limit) + " transactions for " + good + ":")
+        for tran in transactions:
+            print("Tid: ", tran['tid'], " -  Type: ", tran['type'], " -  Price: ", tran['price'], " -  Amount: ",
+                  tran['amount'], " -  Date: ", datetime.fromtimestamp(tran['date']))
 
 
 def showTransactionsForCurrencies(currencies, count):
@@ -23,5 +28,27 @@ def showTransactionsForCurrencies(currencies, count):
         _getLastTransactions(curr, count)
 
 
+def calculateMean(arr):
+    return sum(arr) / len(arr)
+
+
+def showPriceDifferenceStream(good):
+    transactions = _getApiResponse(good)
+    if transactions:
+        sellPrices = []
+        buyPrices = []
+        for tran in transactions:
+            if tran['type'] == 'sell':
+                sellPrices.append(tran['price'])
+            else:
+                buyPrices.append(tran['price'])
+        meanBuyPrice = calculateMean(buyPrices)
+        meanSellPrice = calculateMean(sellPrices)
+        diff = 1 - (meanBuyPrice - meanSellPrice) / meanSellPrice
+        print("Roznica kupno - sprzedaż = ", diff)
+
+
 myCurrencies = ["BTC", "ETH", "ZEC"]
 showTransactionsForCurrencies(myCurrencies, 10)
+
+showPriceDifferenceStream("BTC")
