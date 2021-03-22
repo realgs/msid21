e529865c56af11ff838dd04c6b9882d0859a11db
gorbart <bartek.go3@gmail.com>
2021-03-22 14:54:50 +0100

    created datastream and buy-sell ratio calculations

--- cryptos.py
+++ cryptos.py
@@ -1,32 +1,58 @@
+import time
+
 import requests
 
 
 class Offer:
-    def __init__(self, market, transaction_type, quantity, rate):
+    def __init__(self, market, transaction_type, quantity, price):
         self.market = market
         self.transaction_type = transaction_type
         self.quantity = quantity
-        self.rate = rate
+        self.price = price
 
     def __repr__(self):
-        return f'{self.transaction_type} offer for {self.market}, quantity = {self.quantity}, price = {self.rate}'
+        return f'{self.transaction_type} offer for {self.market}, quantity = {self.quantity}, price = {self.price}'
 
 
-def get_orderlist(markets):
+def get_offerlist(markets):
     offers = []
     for market in markets:
         market_json = requests.get('https://api.bittrex.com/api/v1.1/public/getorderbook?',
                                            {'market': market, 'type': 'both'}).json()['result']
         for transaction_type in ['buy', 'sell']:
-            for offer in market_json['buy']:
+            for offer in market_json[transaction_type]:
                 offers.append(Offer(market=market, transaction_type=transaction_type, quantity=offer['Quantity'],
-                                    rate=offer['Rate']))
+                                    price=offer['Rate']))
 
     return offers
 
 
-def print_orderlist(orderlist):
-    for order in orderlist:
-        print(order)
+def print_list(ls):
+    for item in ls:
+        print(item)
 
 
+def count_ratios(markets, offers):
+    res_list = []
+    for market_name in markets:
+        temp_offer_list = list(filter(lambda offer: offer.market == market_name, offers))
+        buy_offer_list = list(filter(lambda offer: offer.transaction_type == 'buy', temp_offer_list))
+        sell_offer_list = list(filter(lambda offer: offer.transaction_type == 'sell', temp_offer_list))
+        counts_number = len(buy_offer_list) if len(buy_offer_list) <= len(sell_offer_list) else sell_offer_list
+        for i in range(counts_number):
+            result = 1 - (sell_offer_list[i].price - buy_offer_list[i].price) / buy_offer_list[i].price
+            res_list.append(f'{market_name} buy - sell ratio: ' + '{:.1f}%'.format(result*100) +
+                            f' for sell price {sell_offer_list[i].price} and buy price {buy_offer_list[i].price}')
+
+    return res_list
+
+
+def get_datastream(markets):
+    while True:
+        offers = get_offerlist(markets)
+        print_list(offers)
+        print('\n')
+        ratios = count_ratios(markets, offers)
+        print_list(ratios)
+        print('\n')
+        time.sleep(5)
--- tests.py
+++ tests.py
@@ -1,5 +1,16 @@
-from cryptos import get_orderlist, print_orderlist
+from cryptos import get_offerlist, print_list, get_datastream, count_ratios
 
-market_list = ['USD-BTC', 'USD-LTC', 'USD-ETH']
-order_list = get_orderlist(market_list)
-print_orderlist(order_list)
+market_list = ['USD-BTC']
+
+
+def zad1test():
+    order_list = get_offerlist(market_list)
+    print_list(order_list)
+    print(count_ratios(market_list, order_list))
+
+
+def zad2test():
+    get_datastream(market_list)
+
+
+zad2test()
