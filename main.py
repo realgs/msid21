03db0502baaf389b12cee83a7980d1f31b74602e
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-03-22 13:48:41 +0100

    Coded exercise 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 
 API = "https://bitbay.net/API/Public/"
 
+
 def requestAPI(url):
     response = requests.get(url)
 
@@ -11,34 +12,71 @@ def requestAPI(url):
         print(response.reason)
         return None
 
-def getOrders(cryptocurrency, currency):
+
+def getOrders(cryptocurrency, currency, limit=10):
     url = f'{API}{cryptocurrency}{currency}/orderbook.json'
-    response = requestAPI(url)
-    if response != None:
-        printOrders(cryptocurrency, currency, response)
+    orders = requestAPI(url)
+    if orders != None:
+        return {'bids': orders['bids'][:limit], 'asks': orders['asks'][:limit]}
+    return None
 
-def printOrders(cryptocurrency, currency, orders):
-    sellOrders = orders['bids']
-    buyOrders = orders['asks']
 
-    print(f'{cryptocurrency}:{currency} | BUY ORDERS:')
-    for order in buyOrders:
-        printOrder(cryptocurrency, currency, order)
+def printOrders(cryptocurrency, currency, limit = 4):
+    orders = getOrders(cryptocurrency, currency, limit)
+    if orders != None:
+        sellOrders = orders['bids']
+        buyOrders = orders['asks']
 
-    print(f'{cryptocurrency}:{currency} | SELL ORDERS:')
-    for order in sellOrders:
-        printOrder(cryptocurrency, currency, order)    
+        print(f'{cryptocurrency}:{currency} | BUY ORDERS:')
+        for order in buyOrders:
+            printOrder(cryptocurrency, currency, order)
+
+        print(f'{cryptocurrency}:{currency} | SELL ORDERS:')
+        for order in sellOrders:
+            printOrder(cryptocurrency, currency, order)
+
 
 def printOrder(cryptocurrency, currency, order):
-    print(f'{order[1]} {cryptocurrency} for {order[0]} {currency}')
+    print(f'{order[1]} {cryptocurrency} for {order[0] } {currency}')
+
+
+def calculateProfit(cryptocurrency, currency, limit=5):
+    orders = getOrders(cryptocurrency, currency, limit)
+    if orders != None:
+        sellOrders = orders['bids']
+        buyOrders = orders['asks']
+        sumOfBuyPrice = 0
+        sumOfSellPrice = 0
+        length = min(len(buyOrders), len(sellOrders))
+
+        for index in range(length):
+            sumOfBuyPrice = sumOfBuyPrice + (buyOrders[index][0])
+            sumOfSellPrice = sumOfSellPrice + (sellOrders[index][0])
+
+        averageBuyPrice = sumOfBuyPrice/length
+        averageSellPrice = sumOfSellPrice/length
+        profit = 1 - (averageSellPrice - averageBuyPrice) / averageBuyPrice * 100
+        print(f'Average profit on: {cryptocurrency} = {profit:.3}%')
+
 
 def ex1():
-    getOrders('BTC', 'USD')
-    getOrders('LTC', 'USD')
-    getOrders('DASH', 'USD')
+    printOrders('BTC', 'USD')
+    printOrders('LTC', 'USD')
+    printOrders('DASH', 'USD')
+
+
+def ex2():
+    calculateProfit('BTC', 'USD')
+    calculateProfit('LTC', 'USD')
+    calculateProfit('DASH', 'USD')
+
 
 def main():
+    print("Exercise 1:")
     ex1()
+    print("Exercise 2:")
+    ex2()
+
 
 if __name__ == "__main__":
     main()
