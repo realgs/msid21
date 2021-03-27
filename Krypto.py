64d53da83a88e0a5593785de194cee70460563de
Kleinowski Mateusz <0kleinowski0@gmail.com>
2021-03-27 19:28:18 +0100

    Empty file

---
+++ Krypto.py
@@ -0,0 +1,11 @@
+import requests
+
+API = "https://api.bittrex.com/api/v1.1/public/getticker?market="
+currencies = ['USD-BTC', 'USD-ETH', 'USD-LTC']
+
+def main():
+        print()
+
+
+if __name__ == '__main__':
+    main()
