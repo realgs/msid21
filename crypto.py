d72ccc5117afefd6ed573402f5ac33ecd7aecc11
Witek <hello>
2021-03-28 11:23:05 +0200

    added profit calculation

--- crypto.py
+++ crypto.py
@@ -1,15 +1,19 @@
 import requests
+import time
+
+DELAY_OF_EXPLORING_DATA = 20
+LIMIT = 5
 
 
-def connectToCryptoApi(currency1, currency2):
-    response = requests.get('https://bitbay.net/API/Public/' + currency1 + currency2 + '/orderbook.json')
+def connectToCryptoApi(crypto, currency):
+    response = requests.get('https://bitbay.net/API/Public/' + crypto + currency + '/orderbook.json')
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
 
-def printCryptoOffers(jsonResponse, crypto, currency, limit=5):
+def printCryptoOffers(jsonResponse, crypto, currency, limit):
     if jsonResponse is not None:
         print(crypto + '/' + currency + ', BUY OFFERS:')
         print('[RATE, AMOUNT]')
@@ -21,10 +25,36 @@ def printCryptoOffers(jsonResponse, crypto, currency, limit=5):
             print(sellOffer)
 
 
+def calculateProfit(crypto, currency, limit):
+    jsonResponse = connectToCryptoApi(crypto, currency)
+    sumOfBuyPrice = 0
+    sumOfSellPrice = 0
+    if jsonResponse is not None:
+        for buyOffer in jsonResponse['bids'][:limit]:
+            sumOfBuyPrice = sumOfBuyPrice + buyOffer[0]
+        for sellOffer in jsonResponse['asks'][:limit]:
+            sumOfSellPrice = sumOfSellPrice + sellOffer[0]
+    averageOfBuyPrice = sumOfBuyPrice / limit
+    averageOfSellPrice = sumOfSellPrice / limit
+    profit = (1 - (averageOfSellPrice - averageOfBuyPrice) / averageOfBuyPrice) * 100
+    return profit
+
+
+def showProfit(crypto, currency, delayOfExploringData, limit):
+    while True:
+        profit = calculateProfit(crypto, currency, limit)
+        print('Profit: ' + crypto + '/' + currency + ': ', profit)
+        time.sleep(delayOfExploringData)
+
+
 def main():
-    printCryptoOffers(connectToCryptoApi('BTC', 'USD'), 'BTC', 'USD')
-    printCryptoOffers(connectToCryptoApi('LTC', 'USD'), 'LTC', 'USD')
-    printCryptoOffers(connectToCryptoApi('DASH', 'USD'), 'DASH', 'USD')
+    printCryptoOffers(connectToCryptoApi('BTC', 'USD'), 'BTC', 'USD', LIMIT)
+    printCryptoOffers(connectToCryptoApi('LTC', 'USD'), 'LTC', 'USD', LIMIT)
+    printCryptoOffers(connectToCryptoApi('DASH', 'USD'), 'DASH', 'USD', LIMIT)
+
+    #showProfit('BTC', 'USD', DELAY_OF_EXPLORING_DATA, 1)
+    showProfit('LTC', 'USD', DELAY_OF_EXPLORING_DATA, 1)
+    #showProfit('DASH', 'USD', DELAY_OF_EXPLORING_DATA, 1)
 
 
 if __name__ == '__main__':
