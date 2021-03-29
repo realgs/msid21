94b49066c5f9c861fea924d82be8e766bc0fc460
Michal Najwer <254560@student.pwr.edu.pl>
2021-03-29 10:45:01 +0200

    Refactoring

--- L2/main.py
+++ L2/main.py
@@ -2,40 +2,22 @@ import threading
 import time
 import requests
 
-REPLIES = 3
+
+def print_bids_asks(curr1, curr2, replies):
+    data = requests.get("https://bitbay.net/API/Public/"+curr1+curr2+"/orderbook.json").json()
+
+    print(curr1, "bids:")
+    for i in range(replies):
+        print(data['bids'][i][0], curr2)
+
+    print(curr1, "asks:")
+    for i in range(replies):
+        print(data['asks'][i][0], curr2)
+
+# print_bids_asks(curr1, curr2, replies)
 
 
-def print_bids_asks():
-    data_btc = requests.get("https://bitbay.net/API/Public/BTCUSD/orderbook.json").json()
-    data_ltc = requests.get("https://bitbay.net/API/Public/LTCUSD/orderbook.json").json()
-    data_dash = requests.get("https://bitbay.net/API/Public/DASHUSD/orderbook.json").json()
-
-    print("BTC bids:")
-    for i in range(REPLIES):
-        print(data_btc['bids'][i][0], "$")
-
-    print("BTC asks:")
-    for i in range(REPLIES):
-        print(data_btc['asks'][i][0], "$")
-
-    print("LTC bids:")
-    for i in range(REPLIES):
-        print(data_ltc['bids'][i][0], "$")
-
-    print("LTC asks:")
-    for i in range(REPLIES):
-        print(data_ltc['asks'][i][0], "$")
-
-    print("DASH bids:")
-    for i in range(REPLIES):
-        print(data_dash['bids'][i][0], "$")
-
-    print("DASH asks:")
-    for i in range(REPLIES):
-        print(data_dash['asks'][i][0], "$")
-
-
-def print_bids_asks_loop():
+def print_bids_asks_loop(curr1):
 
     exit_event = threading.Event()
 
@@ -43,21 +25,34 @@ def print_bids_asks_loop():
         while True:
             if exit_event.isSet():
                 break
-            data_btc = requests.get("https://bitbay.net/API/Public/BTCUSD/orderbook.json").json()
+            data_btc = requests.get("https://bitbay.net/API/Public/"+curr1+"USD/orderbook.json").json()
             bid = data_btc['bids'][0][0]
             ask = data_btc['asks'][0][0]
 
             print(str((1-((ask-bid)/bid))*100)+"%")
             time.sleep(5)
 
-    print("Printing bids and asks diff % for BTC in a loop\nPress enter to exit the aplication")
+    print("Printing bids and asks diff % for", curr1, "in a loop\nPress enter to exit the aplication")
     th = threading.Thread(target=print_loop)
     th.start()
     input()
     exit_event.set()
     print("Stop")
 
+# print_bids_asks_loop(curr1)
+
+
+def ex1():
+    replies = 3
+    print_bids_asks("BTC", "USD", replies)
+    print_bids_asks("LTC", "USD", replies)
+    print_bids_asks("DASH", "USD", replies)
+
+# ex1()
+
 
 if __name__ == '__main__':
-    print_bids_asks()
-    print_bids_asks_loop()
+    ex1()
+    print_bids_asks_loop("BTC")
+
+# main
