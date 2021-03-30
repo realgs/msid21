eea5ae657a3e427d352cf63f9e72bf028f3effc2
Łukasz Groszek <grochu4@o2.pl>
2021-03-31 01:25:28 +0200

    Zadanie 1 - refactoring

--- FinancialDataApi/orderbook.py
+++ FinancialDataApi/orderbook.py
@@ -1,54 +1,64 @@
 import requests
 
-COLUMN_SIZE = 16
-PRECISION = 4
 MARKETS = ["BTCUSD", "LTCUSD", "DASHUSD"]
-TABLE_SEPARATOR = "|"
+PRECISION = 4
 MAX_RECORDS = 20
+MIN_COLUMN_SIZE = 16
+TABLE_SEPARATOR = "|"
 
-def printTableHeaders():
-    print(
-        TABLE_SEPARATOR +
-        f"{'BIDS':^{COLUMN_SIZE * 2 + len(TABLE_SEPARATOR)}}" + 
-        TABLE_SEPARATOR + TABLE_SEPARATOR +
-        f"{'ASKS':^{COLUMN_SIZE * 2 + len(TABLE_SEPARATOR)}}" +
-        TABLE_SEPARATOR + "\n" +
 
-        TABLE_SEPARATOR +
-        f"{'Rate':^{COLUMN_SIZE}}"      + TABLE_SEPARATOR + 
-        f"{'Amount':^{COLUMN_SIZE}}"    + 
-        TABLE_SEPARATOR + TABLE_SEPARATOR +
-        f"{'Rate':^{COLUMN_SIZE}}"      + TABLE_SEPARATOR + 
-        f"{'Amount':^{COLUMN_SIZE}}"    + 
-        TABLE_SEPARATOR
-    )
+
 
 def printTableRow(bid, ask):
     print(  
         TABLE_SEPARATOR +
-        f"{float(bid[0]):{COLUMN_SIZE}.{PRECISION}f}" + TABLE_SEPARATOR +
-        f"{float(bid[1]):{COLUMN_SIZE}.{PRECISION}f}" + 
+        f"{float(bid[0]):{MIN_COLUMN_SIZE}.{PRECISION}f}" + TABLE_SEPARATOR +
+        f"{float(bid[1]):{MIN_COLUMN_SIZE}.{PRECISION}f}" + 
         TABLE_SEPARATOR + TABLE_SEPARATOR +
-        f"{float(ask[0]):{COLUMN_SIZE}.{PRECISION}f}" + TABLE_SEPARATOR +
-        f"{float(ask[1]):{COLUMN_SIZE}.{PRECISION}f}" + 
+        f"{float(ask[0]):{MIN_COLUMN_SIZE}.{PRECISION}f}" + TABLE_SEPARATOR +
+        f"{float(ask[1]):{MIN_COLUMN_SIZE}.{PRECISION}f}" + 
         TABLE_SEPARATOR
     )
 
-def printMarket(market):
+def printTableHeaders():
+    print(
+        TABLE_SEPARATOR +
+        f"{'BIDS':^{MIN_COLUMN_SIZE * 2 + len(TABLE_SEPARATOR)}}" + 
+        TABLE_SEPARATOR + TABLE_SEPARATOR +
+        f"{'ASKS':^{MIN_COLUMN_SIZE * 2 + len(TABLE_SEPARATOR)}}" +
+        TABLE_SEPARATOR + "\n" +
+
+        TABLE_SEPARATOR +
+        f"{'Rate':^{MIN_COLUMN_SIZE}}"      + TABLE_SEPARATOR + 
+        f"{'Amount':^{MIN_COLUMN_SIZE}}"    + 
+        TABLE_SEPARATOR + TABLE_SEPARATOR +
+        f"{'Rate':^{MIN_COLUMN_SIZE}}"      + TABLE_SEPARATOR + 
+        f"{'Amount':^{MIN_COLUMN_SIZE}}"    + 
+        TABLE_SEPARATOR
+    )
+
+def printOrdersTable(orders):
+    printTableHeaders()
+    bids = orders["bids"]
+    asks = orders["asks"]
+    for i in range(min(len(bids), len(asks), MAX_RECORDS)):
+        printTableRow(bids[i], asks[i])
+    print("\n\n")
+    
+def getOrders(market):
     response = requests.get(f"https://bitbay.net/API/Public/{market}/orderbook.json")
     if response.status_code == 200:
-        printTableHeaders()
-        bids = response.json()["bids"]
-        asks = response.json()["asks"]
-        for i in range(min(len(bids), len(asks), MAX_RECORDS)):
-            printTableRow(bids[i], asks[i])
-        print("\n\n")
+        return response.json()
     else:
-        print(f"Something went wrong (response is : {response.status_code}")
+        return None
 
-def printAllMarkets(markets):
+def printOrdersForEachMarket(markets):
     for market in markets:
-        print(f"\tORDERBOOK FOR {market}:")
-        printMarket(market)
-
-printAllMarkets(MARKETS)
+        orders = getOrders(market)
+        if orders:
+            print(f"\tORDERBOOK FOR {market}:")
+            printOrdersTable(orders)
+        else:
+            print(f"Something went wrong. Cannot get orders")
+                
+printOrdersForEachMarket(MARKETS)
