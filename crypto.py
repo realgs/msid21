27dae97481bbec07c1785c2d8d741d4458140dc8
Witek <hello>
2021-03-31 17:47:11 +0200

    removed print from difference calculation function

--- crypto.py
+++ crypto.py
@@ -3,11 +3,12 @@ import time
 
 DELAY_OF_EXPLORING_DATA = 5
 LIMIT = 5
+RATE_AMOUNT_TEXT = '[RATE, AMOUNT]'
 
 
 def connectToCryptoApi(crypto, currency):
     response = requests.get('https://bitbay.net/API/Public/' + crypto + currency + '/orderbook.json')
-    if response.status_code == 200:
+    if response.status_code in (200, 299):
         return response.json()
     else:
         return None
@@ -16,33 +17,29 @@ def connectToCryptoApi(crypto, currency):
 def printCryptoOffers(jsonResponse, crypto, currency, limit):
     if jsonResponse is not None:
         print(crypto + '/' + currency + ', BUY OFFERS:')
-        print('[RATE, AMOUNT]')
+        print(RATE_AMOUNT_TEXT)
         for buyOffer in jsonResponse['bids'][:limit]:
             print(buyOffer)
         print(crypto + '/' + currency + ', SELL OFFERS:')
-        print('[RATE, AMOUNT]')
+        print(RATE_AMOUNT_TEXT)
         for sellOffer in jsonResponse['asks'][:limit]:
             print(sellOffer)
 
 
-def calculateDifferenceRatio(crypto, currency, limit=10):
+def calculateDifferenceRatio(crypto, currency):
     jsonResponse = connectToCryptoApi(crypto, currency)
     if jsonResponse is not None:
-        buyOffers = jsonResponse['bids'][:limit]
-        sellOffers = jsonResponse['asks'][:limit]
-        buyOffersCount = len(buyOffers)
-        sellOffersCount = len(sellOffers)
-        offersCount = min(buyOffersCount, sellOffersCount)
-        for offerIndex in range(offersCount):
-            buyPrice = buyOffers[offerIndex][0]
-            sellPrice = sellOffers[offerIndex][0]
-            differenceRatio = 1 - (sellPrice - buyPrice) / buyPrice
-            print('Difference ratio in %: ' + crypto + '/' + currency + ': ', differenceRatio)
+        buyOffers = jsonResponse['bids']
+        sellOffers = jsonResponse['asks']
+        buyPrice = buyOffers[0][0]
+        sellPrice = sellOffers[0][0]
+        differenceRatio = 1 - (sellPrice - buyPrice) / buyPrice
+    return differenceRatio
 
 
-def calcDifference(crypto, currency, delayOfExploringData):
+def printCalcDifference(crypto, currency, delayOfExploringData):
     while True:
-        calculateDifferenceRatio(crypto, currency)
+        print('Difference ratio in %: ' + crypto + '/' + currency + ': ', calculateDifferenceRatio(crypto, currency))
         time.sleep(delayOfExploringData)
 
 
@@ -51,9 +48,9 @@ def main():
     printCryptoOffers(connectToCryptoApi('LTC', 'USD'), 'LTC', 'USD', LIMIT)
     printCryptoOffers(connectToCryptoApi('DASH', 'USD'), 'DASH', 'USD', LIMIT)
 
-    calcDifference('BTC', 'USD', DELAY_OF_EXPLORING_DATA)
-    # calcDifference('LTC', 'USD', DELAY_OF_EXPLORING_DATA)
-    # calcDifference('DASH', 'USD', DELAY_OF_EXPLORING_DATA)
+    # printCalcDifference('BTC', 'USD', DELAY_OF_EXPLORING_DATA)
+    printCalcDifference('LTC', 'USD', DELAY_OF_EXPLORING_DATA)
+    # printCalcDifference('DASH', 'USD', DELAY_OF_EXPLORING_DATA)
 
 
 if __name__ == '__main__':
