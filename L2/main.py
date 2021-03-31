1af8fc9a5bfd4e59b2bd2da74f924d18468bd0b1
Michal Najwer <254560@student.pwr.edu.pl>
2021-03-31 17:31:32 +0200

    Quick fix

--- L2/main.py
+++ L2/main.py
@@ -2,6 +2,10 @@ import threading
 import time
 import requests
 
+SLEEP = 5
+REPLIES = 3
+BASE_CURRENCY = "USD"
+
 
 def print_bids_asks(curr1, curr2, replies):
     data = requests.get("https://bitbay.net/API/Public/"+curr1+curr2+"/orderbook.json").json()
@@ -25,12 +29,12 @@ def print_bids_asks_loop(curr1):
         while True:
             if exit_event.isSet():
                 break
-            data_btc = requests.get("https://bitbay.net/API/Public/"+curr1+"USD/orderbook.json").json()
+            data_btc = requests.get("https://bitbay.net/API/Public/"+curr1+BASE_CURRENCY+"/orderbook.json").json()
             bid = data_btc['bids'][0][0]
             ask = data_btc['asks'][0][0]
 
             print(str((1-((ask-bid)/bid))*100)+"%")
-            time.sleep(5)
+            time.sleep(SLEEP)
 
     # print_loop()
 
@@ -45,10 +49,9 @@ def print_bids_asks_loop(curr1):
 
 
 def ex1():
-    REPLIES = 3
-    print_bids_asks("BTC", "USD", REPLIES)
-    print_bids_asks("LTC", "USD", REPLIES)
-    print_bids_asks("DASH", "USD", REPLIES)
+    print_bids_asks("BTC", BASE_CURRENCY, REPLIES)
+    print_bids_asks("LTC", BASE_CURRENCY, REPLIES)
+    print_bids_asks("DASH", BASE_CURRENCY, REPLIES)
 
 # ex1()
 
