5c88d7b08eff260022a3c6a1a4a1b2dcbc9cf9bf
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-04-01 11:33:56 +0200

    Extract seeker class

--- finance.py
+++ finance.py
@@ -1,102 +1,100 @@
-import bitbay
-import bittrex
 import time
 from _datetime import datetime
 
-# TODO: Check fee policy
-FIRST_API_NAME = bittrex.NAME
-SECOND_API_NAME = bitbay.NAME
+SUCCESS_KEY = 'success'
 
 
-def _displayRate(firstApiData, secondApiData, source, message):
-    firstApiBuy = firstApiData[source]['price']
-    secondApiBuy = secondApiData[source]['price']
-    rateFixed = "{:.6f}".format(firstApiBuy / secondApiBuy * 100)
-    print(f"{message}: {FIRST_API_NAME} / {SECOND_API_NAME} = {rateFixed} %")
+class ProfitSeeker:
+    def __init__(self, firstApi, secondApi):
+        self.firstApi = firstApi
+        self.secondApi = secondApi
+        self.firstApiName = firstApi.getName()
+        self.secondApiName = secondApi.getName()
 
+    def _displayRate(self, firstApiData, secondApiData, source, message):
+        firstApiBuy = firstApiData[source]['price']
+        secondApiBuy = secondApiData[source]['price']
+        rateFixed = "{:.6f}".format(firstApiBuy / secondApiBuy * 100)
+        print(f"{message}: {self.firstApiName} / {self.secondApiName} = {rateFixed} %")
 
-def _displayBuyRate(firstApiData, secondApiData):
-    _displayRate(firstApiData, secondApiData, 'buy', 'Buy rate')
+    def _displayBuyRate(self, firstApiData, secondApiData):
+        self._displayRate(firstApiData, secondApiData, 'buy', 'Buy rate')
 
+    def displayBuyRate(self, cryptos):
+        bestFirst = self.firstApi.getBestOrders(cryptos)
+        bestSecond = self.secondApi.getBestOrders(cryptos)
 
-def displayBuyRate(cryptos):
-    bestBittrex = bittrex.getBestOrders(cryptos)
-    bestBitbay = bitbay.getBestOrders(cryptos)
+        if bestFirst[SUCCESS_KEY] and bestSecond[SUCCESS_KEY]:
+            self._displayBuyRate(bestFirst, bestSecond)
+        else:
+            print("The rate cannot be calculated")
 
-    if bestBittrex['success'] and bestBitbay['success']:
-        _displayBuyRate(bestBittrex, bestBitbay)
-    else:
-        print("The rate cannot be calculated")
+    def _displaySellRate(self, firstApiData, secondApiData):
+        self._displayRate(firstApiData, secondApiData, 'sell', 'Sell rate')
 
+    def displaySellRate(self, cryptos):
+        bestFirst = self.firstApi.getBestOrders(cryptos)
+        bestSecond = self.secondApi.getBestOrders(cryptos)
 
-def _displaySellRate(firstApiData, secondApiData):
-    _displayRate(firstApiData, secondApiData, 'sell', 'Sell rate')
+        if bestFirst[SUCCESS_KEY] and bestSecond[SUCCESS_KEY]:
+            self._displayBuyRate(bestFirst, bestSecond)
+        else:
+            print("The rate cannot be calculated")
 
+    @staticmethod
+    def _getTransactionData(buyOrder, sellOrder, market1TakerFee, market2TakerFee, market2TransferFee):
+        rate = buyOrder['price'] * (1 - market1TakerFee) / sellOrder['price'] * (1 - market2TakerFee)
+        quantity = min(buyOrder['quantity'], sellOrder['quantity'])
+        priceDiffer = (rate - 1) * quantity
+        exchangeProfit = priceDiffer - priceDiffer * (market1TakerFee + market2TakerFee)
+        withdrewProfit = exchangeProfit - market2TransferFee
+        return rate, quantity, withdrewProfit
 
-def displaySellRate(cryptos):
-    bestBittrex = bittrex.getBestOrders(cryptos)
-    bestBitbay = bitbay.getBestOrders(cryptos)
+    @staticmethod
+    def _printFullInfo(buyIn, sellIn, rate, quantity, profit, currency):
+        rateFixed = "{:.6f}".format(rate * 100)
+        quantityFixed = "{:.6f}".format(quantity)
+        if profit > 0:
+            colorPrefix, colorSuffix = '\x1b[6;30;42m', '\x1b[0m'
+        else:
+            colorPrefix, colorSuffix = '\x1b[6;30;41m', '\x1b[0m'
+        print(f"{colorPrefix}Buy in {buyIn}, sell in {sellIn}\t\tRate: {rateFixed},"
+              f"\t\tQuantity: {quantityFixed},\t\tFull profit: {profit} {currency}{colorSuffix}")
 
-    if bestBittrex['success'] and bestBitbay['success']:
-        _displayBuyRate(bestBittrex, bestBitbay)
-    else:
-        print("The rate cannot be calculated")
+    def _displayCrossProfitRate(self, firstApiData, secondApiData, currency):
+        firstApiBuy, firstApiSell = firstApiData['buy'], firstApiData['sell']
+        secondApiBuy, secondApiSell = secondApiData['buy'], secondApiData['sell']
 
+        rate1, quantity1, profit1 = self._getTransactionData(firstApiBuy, secondApiSell, self.firstApi.getTakerFee(),
+                                                             self.secondApi.getTakerFee(), self.secondApi.getTransferFee(currency))
+        rate2, quantity2, profit2 = self._getTransactionData(secondApiBuy, firstApiSell, self.secondApi.getTakerFee(),
+                                                             self.firstApi.getTakerFee(), self.firstApi.getTransferFee(currency))
 
-def _getTransactionData(buyOrder, sellOrder, firstMarketTakerFee, secondMarketTakerFee, secondMarketTransferFee):
-    rate = buyOrder['price'] * (1 - firstMarketTakerFee) / sellOrder['price'] * (1 - secondMarketTakerFee)
-    quantity = min(buyOrder['quantity'], sellOrder['quantity'])
-    priceDiffer = (rate - 1) * quantity
-    exchangeProfit = priceDiffer - priceDiffer * (firstMarketTakerFee + secondMarketTakerFee)
-    withdrewProfit = exchangeProfit - secondMarketTransferFee
-    return rate, quantity, withdrewProfit
+        print("Profit percentage (100% - 0 profit):")
+        self._printFullInfo(self.firstApiName, self.secondApiName, rate1, quantity1, profit1, currency)
+        self._printFullInfo(self.secondApiName, self.firstApiName, rate2, quantity2, profit2, currency)
 
+    def displayCrossProfitRate(self, cryptos):
+        bestFirst = self.firstApi.getBestOrders(cryptos)
+        bestSecond = self.secondApi.getBestOrders(cryptos)
 
-def _printFullInfo(buyIn, sellIn, rate, quantity, profit, currency):
-    rateFixed = "{:.6f}".format(rate * 100)
-    quantityFixed = "{:.6f}".format(quantity)
-    if profit > 0:
-        colorPrefix, colorSuffix = '\x1b[6;30;42m', '\x1b[0m'
-    else:
-        colorPrefix, colorSuffix = '\x1b[6;30;41m', '\x1b[0m'
-    print(f"{colorPrefix}Buy in {buyIn}, sell in {sellIn}\t\tRate: {rateFixed},"
-          f"\t\tQuantity: {quantityFixed},\t\tFull profit: {profit} {currency}{colorSuffix}")
+        if bestFirst[SUCCESS_KEY] and bestSecond[SUCCESS_KEY]:
+            self._displayCrossProfitRate(bestFirst, bestSecond, cryptos[1])
+        else:
+            print("The profit rate cannot be calculated")
 
+    def displayMarketsDifferenceRateStream(self, allCryptos, interval=5):
+        while True:
+            for cryptos in allCryptos:
+                print("\n", datetime.now().strftime("%H:%M:%S"), f"{cryptos[0]} -> {cryptos[1]}")
+                bestFirst = self.firstApi.getBestOrders(cryptos)
+                bestSecond = self.secondApi.getBestOrders(cryptos)
 
-def _displayCrossProfitRate(firstApiData, secondApiData, currency):
-    firstApiBuy, firstApiSell = firstApiData['buy'], firstApiData['sell']
-    secondApiBuy, secondApiSell = secondApiData['buy'], secondApiData['sell']
-
-    rate1, quantity1, profit1 = _getTransactionData(firstApiBuy, secondApiSell, bittrex.getTakerFee(), bitbay.getTakerFee(), bitbay.getTransferFee(currency))
-    rate2, quantity2, profit2 = _getTransactionData(secondApiBuy, firstApiSell, bitbay.getTakerFee(), bittrex.getTakerFee(), bittrex.getTransferFee(currency))
-
-    print("Profit percentage (100% - 0 profit):")
-    _printFullInfo(FIRST_API_NAME, SECOND_API_NAME, rate1, quantity1, profit1, currency)
-    _printFullInfo(SECOND_API_NAME, FIRST_API_NAME, rate2, quantity2, profit2, currency)
-
-
-def displayCrossProfitRate(cryptos):
-    bestBittrex = bittrex.getBestOrders(cryptos)
-    bestBitbay = bitbay.getBestOrders(cryptos)
-
-    if bestBittrex['success'] and bestBitbay['success']:
-        _displayCrossProfitRate(bestBittrex, bestBitbay, cryptos[1])
-    else:
-        print("The profit rate cannot be calculated")
-
-
-def displayMarketsDifferenceRateStream(allCryptos, interval=5):
-    while True:
-        for cryptos in allCryptos:
-            print("\n", datetime.now().strftime("%H:%M:%S"), f"{cryptos[0]} -> {cryptos[1]}")
-            bestBittrex = bittrex.getBestOrders(cryptos)
-            bestBitbay = bitbay.getBestOrders(cryptos)
-
-            if bestBittrex['success'] and bestBitbay['success']:
-                _displayBuyRate(bestBittrex, bestBitbay)
-                _displaySellRate(bestBittrex, bestBitbay)
-                _displayCrossProfitRate(bestBittrex, bestBitbay, cryptos[1])
-            else:
-                print("The rate cannot be calculated")
-                return
-        time.sleep(interval)
+                if bestFirst[SUCCESS_KEY] and bestSecond[SUCCESS_KEY]:
+                    self._displayBuyRate(bestFirst, bestSecond)
+                    self._displaySellRate(bestFirst, bestSecond)
+                    self._displayCrossProfitRate(bestFirst, bestSecond, cryptos[1])
+                else:
+                    print("The rate cannot be calculated")
+                    return
+            time.sleep(interval)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import finance
+from finance import ProfitSeeker
+import bittrex
+import bitbay
 
 cryptos = [("BTC", "ETH"), ("USD", "BTC"), ("ETH", "LTC")]
 
-finance.displayMarketsDifferenceRateStream(cryptos)
+bittrexBitbaySeeker = ProfitSeeker(bittrex, bitbay)
+
+bittrexBitbaySeeker.displayMarketsDifferenceRateStream(cryptos)
