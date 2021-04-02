c2456739e28ded1fa71166a7f7f3c68fa734c4ca
Boguslawa-Tlolka <79993467+Boguslawa-Tlolka@users.noreply.github.com>
2021-04-03 00:15:51 +0200

    Delete Sorting directory

--- Sorting/bubblesort.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(number_array):
-    size = len(number_array)
-    for i in range(1, size):
-        for left in range(0, size - i):
-            right = left + 1
-            if number_array[left] >= number_array[right]:
-                number_array[left], number_array[right] = number_array[right], number_array[left]
-    return number_array
