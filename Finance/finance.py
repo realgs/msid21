ce2341966db7eb062e1a3b002cde430ee6593f47
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2021-04-03 16:58:52 +0200

    Add task2 finance.py

--- Finance/finance.py
+++ Finance/finance.py
@@ -1,10 +1,11 @@
 import json
-
+import time
 import requests
 
 ApiUrl = "https://api.bitbay.net/rest/trading/orderbook-limited/"
 headers = {'content-type': 'application/json'}
 limit10 = 10
+refresh = 5
 btcUsd = "BTC-USD"
 ltcUsd = "LTC-USD"
 dashUsd = "DASH-USD"
@@ -25,6 +26,19 @@ def printJSON(obj):
     print(msg)
 
 
+def calculateDifference(currency):
+    while True:
+        data = getSellBuy(currency, limit10)
+        if data is not None:
+            sell = json.loads(data['sell'][0]['ra'])
+            buy = json.loads(data['buy'][0]['ra'])
+            result = (1 - (sell - buy) / buy) * 100
+            print(f'Difference buy/sell {currency}: {result} %')
+        else:
+            print('There was no data to calculate the difference')
+        time.sleep(refresh)
+
+
 print("BTC-USD: ")
 printJSON(getSellBuy(btcUsd, limit10))
 print("LTC-USD:")
@@ -32,7 +46,6 @@ printJSON(getSellBuy(ltcUsd, limit10))
 print("DASH-USD: ")
 printJSON(getSellBuy(dashUsd, limit10))
 
-
-
-
-
+calculateDifference(btcUsd)
+# calculateDifference(ltcUsd)
+# calculateDifference(dashUsd)
