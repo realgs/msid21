f2081506bfd031b68b9ff8327926d24303df0e40
bedni <w.bednarz2000@gmail.com>
2021-04-04 00:35:59 +0200

    Code refactoring

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,21 @@
 import requests
+from enum import Enum
+
+
+class Operation(Enum):
+    BUY = "bids"
+    SELL = "asks"
+
+
+class Cryptocurrency(Enum):
+    BTC = "BTC"
+    LTC = "LTC"
+    DASH = "DASH"
+
+
+class Currency(Enum):
+    USD = "USD"
+    # PLN = "PLN"
 
 
 def main():
@@ -7,20 +24,19 @@ def main():
 
 
 def task1():
-    print_orders("bids", "BTC", "USD")
-    print_orders("asks", "BTC", "USD")
-    print_orders("bids", "LTC", "USD")
-    print_orders("asks", "LTC", "USD")
-    print_orders("bids", "DASH", "USD")
-    print_orders("asks", "DASH", "USD")
+    for c in Cryptocurrency:
+        print_orders(Operation.BUY, c, Currency.USD)
+        print_orders(Operation.SELL, c, Currency.USD)
 
 
-def print_orders(operation: str, cryptocurrency: str, currency: str, limit: int = 5):
+def print_orders(operation: Operation, cryptocurrency: Cryptocurrency, currency: Currency, limit: int = 5):
+    cryptocurrency = cryptocurrency.value
+    currency = currency.value
     response = requests.get(f"https://bitbay.net/API/Public/{cryptocurrency}{currency}/orderbook.json").json()
 
-    print(f"\n\n# {cryptocurrency}-{currency} {operation} orders\n")
+    print(f"\n\n# {cryptocurrency}-{currency} {operation.name} orders\n")
 
-    for x, i in zip(response[operation], range(1, limit + 1)):
+    for x, i in zip(response[operation.value], range(1, limit + 1)):
         amount = "{:.8f}".format(x[1])
         price = "{:.2f}".format(round(x[0] * x[1], 2))
         print(f"{i}. {amount} {cryptocurrency} - {price} {currency}")
