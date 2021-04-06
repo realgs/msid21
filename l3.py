7ce9506ba6b17ee1bd1647b72e19bf55de7fc16e
Witek <hello>
2021-04-06 11:26:32 +0200

    finished function off getting trade offers from 2 API

--- l3.py
+++ l3.py
@@ -1,7 +1,8 @@
 import requests
 import enum
+
 TRADE_MARKETS = ['bitbay', 'bittrex']
-MARKETS_URL = ['https://bitbay.net/API/Public/', 'https://api.bittrex.com/v3/markets']
+MARKETS_URL = ['https://bitbay.net/API/Public/', 'https://api.bittrex.com/v3/markets/']
 TAKER_FEES = {
     'bitbay': 0.001,
     'bittrex': 0.0025
@@ -22,7 +23,9 @@ TRANSFER_FEES = {
 }
 CRYPTO_CURRENCIES = ['BTC', 'LTC', 'ETH', 'DASH']
 BASE_CURRENCY = 'USD'
+
 OPERATIONS = enum.Enum('BUY', 'SELL', 'ARBITRATION')
+
 def getDataFromApi(url):
     response = requests.get(url)
     if response.status_code in range(200, 299):
@@ -34,24 +37,36 @@ def getDataFromApi(url):
 def getSellBuyOffers(exchangeMarket, cypto, curency, offersLimit):
     if exchangeMarket == TRADE_MARKETS[0]:
         tradeOffers = getDataFromApi(MARKETS_URL[0] + cypto + curency + '/orderbook.json')
-        return tradeOffers[:offersLimit]
+        offers = {'bids': tradeOffers['bids'][:offersLimit], 'asks': tradeOffers['asks'][:offersLimit]}
+        if offers is not None:
+            return offers
     elif exchangeMarket == TRADE_MARKETS[1]:
-        tradeOffers = getDataFromApi(MARKETS_URL[1] + cypto + curency + '/orderbook')
-        return tradeOffers[:offersLimit]
+        tradeOffers = getDataFromApi(MARKETS_URL[1] + cypto + '-' + curency + '/orderbook')
+        sellOffers = []
+        buyOffers = []
+        for bids in tradeOffers['bid'][:offersLimit]:
+            buyOffers.append([float(bids['rate']), float(bids['quantity'])])
+        for asks in tradeOffers['ask'][:offersLimit]:
+            sellOffers.append([float(asks['rate']), float(asks['quantity'])])
+        offers = {'bids': buyOffers, 'asks': sellOffers}
+        if offers is not None:
+            return offers
     return None
+
 
 def findPriceDifference(tradeOffer1, tradeOffer2):
 
 
 def calculatePriceDifference(exchangeMarket, crypto, currency, offersLimit, delayOfExploringData, operation):
-    while True:
-        tradeOffer1 = getSellBuyOffers(exchangeMarket, crypto, currency, offersLimit)
-        tradeOffer2 = getSellBuyOffers(exchangeMarket, crypto, currency, offersLimit)
+ while True:
+      tradeOffer1 = getSellBuyOffers(exchangeMarket, crypto, currency, offersLimit)
+      tradeOffer2 = getSellBuyOffers(exchangeMarket, crypto, currency, offersLimit)
 
-        if operation == OPERATIONS.BUY:
+     if operation == OPERATIONS.BUY:
+          findPriceDifference(tradeOffer1, tradeOffer2)
+     elif operation == OPERATIONS.SELL:
             findPriceDifference(tradeOffer1, tradeOffer2)
-        elif operation == OPERATIONS.SELL:
-            findPriceDifference(tradeOffer1, tradeOffer2)
-        
 
 
+getSellBuyOffers(TRADE_MARKETS[0], 'BTC', 'USD', 10);
+getSellBuyOffers(TRADE_MARKETS[1], 'BTC', 'USD', 10);
