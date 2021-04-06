1921c5f812656afc011a00194e68e7a3de66974f
Michal Najwer <254560@student.pwr.edu.pl>
2021-04-06 23:26:54 +0200

    Updated fees

--- L3/main.py
+++ L3/main.py
@@ -7,10 +7,27 @@ ROUND = 3
 BASE_CURRENCY = "USD"
 URL_BITBAY = "https://bitbay.net/API/Public/{}{}/orderbook.json"
 URL_BITFINEX = "https://api-pub.bitfinex.com/v2/ticker/t{}{}"
-FEE_TAKER_BITBAY = 0.0043
-FEE_TRANSFER_BITBAY = 0.001
-FEE_TAKER_BITFINEX = 0.002
-FEE_TRANSFER_BITFINEX = 0.001
+
+FEE_BITBAY = {
+    "taker": 0.0043,
+    "deposit": 0,
+    "withdrawal": {
+        "BTC": 0.0005,
+        "LTC": 0.001,
+        "ETH": 0.01,
+        "XRP": 0.1
+    }
+}
+FEE_BITFINEX = {
+    "taker": 0.002,
+    "deposit": 0,
+    "withdrawal": {
+        "BTC": 0.0004,
+        "LTC": 0.001,
+        "ETH": 0.013365,
+        "XRP": 0.1
+    }
+}
 
 
 def get_data(url):
@@ -30,7 +47,7 @@ def get_data_bitfinex(curr1, curr2):
     return [data[0], data[1]], [data[2], data[3]]
 
 
-def print_bids_asks_loop(curr1):
+def print_bids_asks_loop(curr):
     exit_event = threading.Event()
 
     def print_loop():
@@ -39,33 +56,39 @@ def print_bids_asks_loop(curr1):
                 break
 
             try:
-                bid_bitfinex, ask_bitfinex = get_data_bitfinex(curr1, BASE_CURRENCY)
-                bid_bitbay, ask_bitbay = get_data_bitbay(curr1, BASE_CURRENCY)
+                bid_bitfinex, ask_bitfinex = get_data_bitfinex(curr, BASE_CURRENCY)
+                bid_bitbay, ask_bitbay = get_data_bitbay(curr, BASE_CURRENCY)
             except AttributeError:
                 print("API Error")
                 return
 
             # asks % diff
-            print("Lowest {} ask difference in %, BitFinex({}) to BitBay({})".format(curr1, ask_bitfinex[0], ask_bitbay[0]))
+            print("Lowest {} ask difference in %, BitFinex({}) to BitBay({})".format(curr, ask_bitfinex[0],
+                                                                                     ask_bitbay[0]))
             print(str(round((ask_bitfinex[0] - ask_bitbay[0]) / ask_bitbay[0] * 100, ROUND)) + "%")
 
             # bids % diff
-            print("Highest {} bid difference in %, BitFinex({}) to BitBay({})".format(curr1, bid_bitfinex[0], bid_bitbay[0]))
+            print("Highest {} bid difference in %, BitFinex({}) to BitBay({})".format(curr, bid_bitfinex[0],
+                                                                                      bid_bitbay[0]))
             print(str(round((bid_bitfinex[0] - bid_bitbay[0]) / bid_bitbay[0] * 100, ROUND)) + "%")
 
             # bid to sell % diff
             bitfinex_to_bitbay = (ask_bitfinex[0] - bid_bitbay[0]) / bid_bitbay[0]
             bitbay_to_bitfinex = (ask_bitbay[0] - bid_bitfinex[0]) / bid_bitfinex[0]
 
-            print("Buying {} on BitFinex({}) to selling on BitBay({})".format(curr1, ask_bitfinex[0], bid_bitbay[0]))
+            print("Buying {} on BitFinex({}) to selling on BitBay({})".format(curr, ask_bitfinex[0], bid_bitbay[0]))
             print(str(round(bitfinex_to_bitbay * 100, ROUND)) + "%")
             if bitfinex_to_bitbay < 0:
-                arbitrage(ask_bitfinex, bid_bitbay, FEE_TAKER_BITFINEX, FEE_TRANSFER_BITFINEX, FEE_TAKER_BITBAY, FEE_TRANSFER_BITBAY)
+                arbitrage(ask_bitfinex, bid_bitbay, FEE_BITFINEX["taker"], FEE_BITFINEX["withdrawal"][curr],
+                          FEE_BITBAY["taker"],
+                          FEE_BITBAY["deposit"])
 
-            print("Buying {} on BitBay({}) to selling on BitFinex({})".format(curr1, ask_bitbay[0], bid_bitfinex[0]))
+            print("Buying {} on BitBay({}) to selling on BitFinex({})".format(curr, ask_bitbay[0], bid_bitfinex[0]))
             print(str(round(bitbay_to_bitfinex * 100, ROUND)) + "%")
             if bitbay_to_bitfinex < 0:
-                arbitrage(ask_bitbay, bid_bitfinex, FEE_TAKER_BITBAY, FEE_TRANSFER_BITBAY, FEE_TAKER_BITFINEX, FEE_TRANSFER_BITFINEX)
+                arbitrage(ask_bitbay, bid_bitfinex, FEE_BITBAY["taker"], FEE_BITBAY["withdrawal"][curr],
+                          FEE_BITFINEX["taker"],
+                          FEE_BITFINEX["deposit"])
 
             time.sleep(SLEEP)
 
@@ -77,23 +100,27 @@ def print_bids_asks_loop(curr1):
     print("Stop")
 
 
-def arbitrage(ask, bid, fee_ask_taker, fee_ask_transfer, fee_bid_taker, fee_bid_transfer):
-    print("Checking arbitrage profitability:")
+def arbitrage(ask, bid, fee_ask_taker, fee_ask_withdrawal, fee_bid_taker, fee_bid_deposit):
     transaction_volume = ask[1] if ask[1] < bid[1] else bid[1]
     buy = transaction_volume * ask[0]
     sell = transaction_volume * bid[0]
     profit = sell - buy
-    buy_fee = buy * fee_ask_taker + buy * fee_ask_transfer
-    sell_fee = sell * fee_bid_taker + buy * fee_bid_transfer
+    buy_fee = buy * fee_ask_taker + fee_ask_withdrawal * ask[0]
+    sell_fee = sell * fee_bid_taker + fee_bid_deposit * bid[0]
     fee = sell_fee + buy_fee
     transaction_cost = profit - fee
-    print("Transaction volume {} can be bought for {}{} and sold for {}{}".format(round(transaction_volume, ROUND), round(buy, ROUND), BASE_CURRENCY, round(sell, ROUND), BASE_CURRENCY))
+    print("Checking arbitrage profitability:")
+    print("Transaction volume {} can be bought for {}{} and sold for {}{}".format(round(transaction_volume, ROUND),
+                                                                                  round(buy, ROUND), BASE_CURRENCY,
+                                                                                  round(sell, ROUND), BASE_CURRENCY))
     print("for a profit of {}{} without fees".format(round(profit, ROUND), BASE_CURRENCY))
-    print("Selling fee {}{}, buying fee {}{}, total fee {}{}".format(round(sell_fee, ROUND), BASE_CURRENCY, round(buy_fee, ROUND), BASE_CURRENCY, round(fee, ROUND), BASE_CURRENCY))
+    print("Selling fee {}{}, buying fee {}{}, total fee {}{}".format(round(sell_fee, ROUND), BASE_CURRENCY,
+                                                                     round(buy_fee, ROUND), BASE_CURRENCY,
+                                                                     round(fee, ROUND), BASE_CURRENCY))
     print("Total transaction cost is {}{}".format(round(transaction_cost, ROUND), BASE_CURRENCY))
     print("what makes it {}".format("profitable" if transaction_cost > 0 else "not profitable"))
     return transaction_cost > 0
 
 
 if __name__ == '__main__':
-    print_bids_asks_loop("LTC")
+    print_bids_asks_loop("XRP")
