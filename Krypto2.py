65929e619d97926e5f4b159293def3b09fb572cf
Kleinowski Mateusz <0kleinowski0@gmail.com>
2021-04-06 21:35:34 +0200

    2 APIs and price difference

--- Krypto2.py
+++ Krypto2.py
@@ -1,25 +1,48 @@
 import requests
 import time
 
-BITTREX_LINK = "https://api.bittrex.com/api/v1.1/public/getorderbook?market=USD-BTC&type=both"
+BITTREX_LINK_START = "https://api.bittrex.com/api/v1.1/public/getorderbook?market="
+BITTREX_LINK_END = "&type=both"
+BITBAY_LINK_START = "https://bitbay.net/API/Public/"
+BITBAY_LINK_END = "/orderbook.json"
 printDelay = 5
 
-# [name, bidPrice, ammountOfBidPrice, askPrice, ammountOfAskPrice]
-def downloadData(currency):
+# [currency, crypto,  [ bidPrice, ammountOfBidPrice, askPrice, ammountOfAskPrice ], ... ]
+def downloadData(currency, crypto):
+    data = [currency, crypto]
     try:
-        infoPack = requests.get(BITTREX_LINK).json()
-        return [currency, infoPack['result']['buy'][0]['Rate'], infoPack['result']['buy'][0]['Quantity'],
-                infoPack['result']['sell'][0]['Rate'], infoPack['result']['sell'][0]['Quantity']]
+        infoPack = requests.get(BITTREX_LINK_START + currency + "-" + crypto + BITTREX_LINK_END).json()
+        data.append( [infoPack['result']['buy'][0]['Rate'], infoPack['result']['buy'][0]['Quantity'],
+                infoPack['result']['sell'][0]['Rate'], infoPack['result']['sell'][0]['Quantity']] )
     except requests.exceptions.ConnectionError:
-        print("Error, can not connect to API + " + infoPack.reason)
+        print("Error, can not connect to BitTrex API + " + infoPack.reason)
         return None
+
+    try:
+        infoPack = requests.get(BITBAY_LINK_START + crypto + currency + BITBAY_LINK_END).json()
+        data.append( [infoPack['bids'][0][0], infoPack['bids'][0][1],
+                infoPack['asks'][1][0], infoPack['asks'][1][1]] )
+    except requests.exceptions.ConnectionError:
+        print("Error, can not connect to BitBay API + " + infoPack.reason)
+        return None
+
+    return data
+
+def printPriceDifference(data):
+    print("BID price difference: " + str((data[2][2]-data[3][0])/data[2][0]*100) + "%")
+    print("ASK price difference: " + str((data[2][2]-data[3][2])/data[2][2]*100) + "%")
+    print()
+    print("BUY in BITTREX, SELL in BITBAY: " + str((data[3][0]-data[2][2])/data[3][0]*100) + "%")
+    print("BUY in BITBAY, SELL in BITTREX: " + str((data[2][0]-data[3][2])/data[2][0]*100) + "%")
+    print()
+    print(str(data[2][0]) + " " + str(data[2][2]) + " " + str(data[3][0]) + " "+str(data[3][2]))
+
 
 def main():
     while 1 > 0:
-        data = downloadData("USD-BTC")
-        print(str(data[0]))
-        print("Bid price: " + str(data[1]) + " (" + str(data[2]) + ")")
-        print("Ask price: " + str(data[3]) + " (" + str(data[4]) + ")")
+        data = downloadData("USD", "BTC")
+        printPriceDifference(data)
+
         time.sleep(printDelay)
 
 
