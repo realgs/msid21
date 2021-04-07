2a65eb76f5963bb97f9b2f8c974a4331157f9af2
jucic1 <julia.cicha60@gmail.com>
2021-04-07 10:53:57 +0200

    First part done and working

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
-# Stworzyć prostą funkcję, która łączy się z danym API, pobiera listę ofert kupna/sprzedaży trzech par zasobów (np. BTCUSD, LTCUSD i DASHUSD)
-# i printuje do konsoli. (5pkt)
-
-# from multipledispatch import dispatch
-
 from FinancialMarketAPI import FinancialMarketAPI
 
+# Stworzyć prostą funkcję, która łączy się z danym API, pobiera listę ofert kupna/sprzedaży trzech par zasobów (np. BTCUSD, LTCUSD i DASHUSD)
+# i printuje do konsoli. (5pkt)
+def first_exc(financialMarketAPI):
+    print(financialMarketAPI.get_transactions("btc", "usd", "trades"))
+    print(financialMarketAPI.get_transactions("ltc", "usd", "trades"))
+    print(financialMarketAPI.get_transactions("dash", "usd", "trades"))
+
+
 financialMarketAPI = FinancialMarketAPI()
-print(financialMarketAPI.get_transactions("btc", "usd", "all"))
+first_exc(financialMarketAPI)
