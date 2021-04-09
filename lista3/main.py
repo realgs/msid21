5b12d74100f1198c139db890f772fd2c2d8c41b1
m-suwala <ancymoncia@gmail.com>
2021-04-10 01:03:38 +0200

    add fees and quantities

--- lista3/main.py
+++ lista3/main.py
@@ -1,10 +1,13 @@
 import requests
 import time
 
+delay = 5
 currency1 = "BTC"
 currency2 = "USD"
 first_api = "https://bitbay.net/API/Public/{}/orderbook.json".format(currency1 + currency2)
-second_api = "https://api.bittrex.com/api/v1.1/public/getticker?market={}-{}".format(currency2, currency1)
+second_api = "https://api.bittrex.com/api/v1.1/public/getorderbook?market={}-{}&type=both".format(currency2, currency1)
+bitbay_fees = {"taker_fee": 0.0042, "BTC_fee": 0.0005}
+bittrex_fees = {"taker_fee": 0.0075, "BTC_fee": 0.0005}
 
 
 def get_offers(api):
@@ -13,20 +16,22 @@ def get_offers(api):
 
 def get_bid_from_bitbay(offer):
     bids = offer.json()['bids']
-    return bids[0][0]
+    return bids[0][0], bids[0][1]
 
 
 def get_ask_from_bitbay(offer):
     asks = offer.json()['asks']
-    return asks[0][0]
+    return asks[0][0], asks[0][1]
 
 
 def get_bid_from_bittrex(offer):
-    return (offer.json()['result'])['Bid']
+    bids = offer.json()['result']['buy']
+    return bids[0]['Rate'], bids[0]['Quantity']
 
 
 def get_ask_from_bittrex(offer):
-    return (offer.json()['result'])['Ask']
+    asks = offer.json()['result']['sell']
+    return asks[0]['Rate'], asks[0]['Quantity']
 
 
 def get_difference_in_percent(offer1, offer2):
@@ -34,10 +39,10 @@ def get_difference_in_percent(offer1, offer2):
 
 
 def print_spread(offer1, offer2):
-    bitbay_bid = get_bid_from_bitbay(offer1)
-    bitbay_ask = get_ask_from_bitbay(offer1)
-    bittrex_bid = get_bid_from_bittrex(offer2)
-    bittrex_ask = get_ask_from_bittrex(offer2)
+    bitbay_bid = get_bid_from_bitbay(offer1)[0]
+    bitbay_ask = get_ask_from_bitbay(offer1)[0]
+    bittrex_bid = get_bid_from_bittrex(offer2)[0]
+    bittrex_ask = get_ask_from_bittrex(offer2)[0]
 
     print(str(get_difference_in_percent(bitbay_bid, bittrex_bid)) + "%")
     print(str(get_difference_in_percent(bitbay_ask, bittrex_ask)) + "%")
@@ -54,4 +59,4 @@ if __name__ == '__main__':
         # print(offers2.json())
         print_spread(offers1, offers2)
         print("----------------------------")
-        time.sleep(5)
+        time.sleep(delay)
