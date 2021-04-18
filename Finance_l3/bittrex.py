63ff78fc4405d87aaf4dc6dc83c3a33b95c66795
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2021-04-18 14:09:46 +0200

    add bittrex.py

--- Finance_l3/bittrex.py
+++ Finance_l3/bittrex.py
@@ -1,18 +1,17 @@
 import requests
 
 
-NAME = "BitBay"
-BITBAY_URL = 'https://api.bitbay.net/rest/trading/orderbook-limited/'
+NAME = "Bittrex"
+BITTREX_URL = "https://api.bittrex.com/api/v1.1/public"
 HEADERS = {'content-type': 'application/json'}
-LIMIT = 10
-BITBAY_FEES = {
-    "taker_fee": 0.0043,
+BITTREX_FEES = {
+    "taker_fee": 0.0035,
     "transfer_fee_btc": 0.0005
 }
 
 
 def getOrderbookData(currency_1, currency_2):
-    url = f'{BITBAY_URL}{currency_1}-{currency_2}/{LIMIT}'
+    url = f'{BITTREX_URL}/getorderbook?market={currency_1}-{currency_2}&type=both'
     response = requests.request("GET", url, headers=HEADERS)
     if response.status_code == requests.codes.ok:
         return response.json()
@@ -24,8 +23,8 @@ def getOrderbookData(currency_1, currency_2):
 def getBestSellBuy(currency_1, currency_2):
     orders = getOrderbookData(currency_1, currency_2)
     if orders:
-        sell = orders['sell'][0]
-        buy = orders['buy'][len(orders['buy']) - 1]
+        sell = orders['result']['sell'][0]
+        buy = orders['result']['buy'][0]
         return sell, buy
     else:
         print("There was no data")
@@ -34,11 +33,11 @@ def getBestSellBuy(currency_1, currency_2):
 
 def printBestSellBuy(sell, buy, currency_1, currency_2):
     if sell and buy:
-        sell_rate = sell['ra']
-        sell_quantity = sell['ca']
-        buy_rate = buy['ra']
-        buy_quantity = buy['ca']
-        print('\nOrders from BitBay:')
+        sell_rate = sell['Rate']
+        sell_quantity = sell['Quantity']
+        buy_rate = buy['Rate']
+        buy_quantity = buy['Quantity']
+        print('Orders from Bittrex:')
         print(f'Lowest sell order for {currency_1}-{currency_2}: rate: {sell_rate}, amount: {sell_quantity}')
         print(f'Highest buy order for {currency_1}-{currency_2}: rate: {buy_rate}, amount: {buy_quantity}\n')
     else:
