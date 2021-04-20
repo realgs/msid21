7c5425cd00484ec0badd6e850ff7aaac4ce0fd31
jucic1 <julia.cicha60@gmail.com>
2021-04-20 13:01:32 +0200

    added 1c)

--- Main.py
+++ Main.py
@@ -8,37 +8,45 @@ SECOND_CURRENCY = "LTC"
 SLEEP_TIME = 5
 
 
-def exc_1(bittRex, FIRST_CURRENCY, SECOND_CURRENCY):
+def exc_1(bitt_rex, first_currency, second_currency):
     print("EXC 1")
     print("a")
-    get_percentage_diff_for_best_buy_sell(bittRex, FIRST_CURRENCY, SECOND_CURRENCY, "buy")
+    get_percentage_diff_for_best_buy_sell(bitt_rex, first_currency, second_currency, "buy")
     print("b")
-    get_percentage_diff_for_best_buy_sell(bittRex, FIRST_CURRENCY, SECOND_CURRENCY, "sell")
+    get_percentage_diff_for_best_buy_sell(bitt_rex, first_currency, second_currency, "sell")
+    print("c")
+    get_percentage_diff_for_best_buy_sell(bitt_rex, first_currency, second_currency, "both")
 
 
-def get_percentage_diff_for_best_buy_sell(bittRex, first_currency, second_currency, type):
-    print(type)
-    if type == "buy" or type == "sell":
-        previousBest = -1
+def get_percentage_diff_for_best_buy_sell(bitt_rex, first_currency, second_currency, type):
+    if type == "buy" or type == "sell" or type == "both":
+        previous_best = -1
         while True:
-            orderBookJSON = bittRex.get_orderbook(first_currency, second_currency, type)
-            if type == "buy":
-                allBuy = orderBookJSON['result']
-                quick_sort_by_rate(allBuy)
-                currentBest = allBuy[len(allBuy) - 1]['Rate']
+            order_book_json = bitt_rex.get_orderbook(first_currency, second_currency, type)
+            if type == "buy" or type == "sell":
+                all_buy_sell = order_book_json['result']
+                quick_sort_by_rate(all_buy_sell)
+                if type == "buy":
+                    current_best = all_buy_sell[len(all_buy_sell) - 1]['Rate']
+                else:
+                    current_best = all_buy_sell[0]['Rate']
             else:
-                allSell = orderBookJSON['result']
-                quick_sort_by_rate(allSell)
-                currentBest = allSell[0]['Rate']
+                all_buy = order_book_json['result']['buy']
+                all_sell = order_book_json['result']['sell']
+                quick_sort_by_rate(all_buy)
+                quick_sort_by_rate(all_sell)
+                best_buy = all_buy[len(all_buy) - 1]['Rate']
+                best_sell = all_sell[0]['Rate']
+                current_best = best_sell - best_buy
 
-            if previousBest == -1: #first round
-                previousBest = currentBest
+            if previous_best == -1:  # first round
+                previous_best = current_best
             else:
-                print(currentBest/previousBest - 1)
-                previousBest = currentBest
+                print(current_best / previous_best - 1)
+                previous_best = current_best
             time.sleep(SLEEP_TIME)
     else:
-        print("Wrong type provided. Type has to be either buy or sell. Not: " + type)
+        print("Wrong type provided. Type has to be either buy, sell or both. Not: " + type)
 
 
 def quick_sort_by_rate(unsorted):
@@ -59,5 +67,5 @@ def quick_sort_by_rate(unsorted):
     return quick_sort_by_rate(lower) + [pivot] + quick_sort_by_rate(greater)
 
 
-bittRex = BittRexAPI()
-exc_1(bittRex, FIRST_CURRENCY, SECOND_CURRENCY)
+bitt_rex = BittRexAPI()
+exc_1(bitt_rex, FIRST_CURRENCY, SECOND_CURRENCY)
