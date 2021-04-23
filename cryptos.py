6e8b3af3a26a531cbe10966b058912d7a513dd1e
gorbart <bartek.go3@gmail.com>
2021-04-23 12:58:10 +0200

    Done finding common markets

--- cryptos.py
+++ cryptos.py
@@ -8,8 +8,12 @@ FEES = {'bittrex': {'taker': 0.25, 'transfer': {'BTC': 0.0005, 'LTC': 0.01, 'ETH
         'bitbay': {'taker': 0.4, 'transfer': {'BTC': 0.0001, 'LTC': 0.1, 'ETH': 0.01}}}
 
 BASE_URLS = {'bittrex': 'https://api.bittrex.com/v3/markets/', 'bitbay': 'https://bitbay.net/API/Public/'}
+GET_MARKETS_URLS = {'bittrex': 'https://api.bittrex.com/v3/markets/',
+                    'bitbay': 'https://api.bitbay.net/rest/trading/ticker'}
+ORDERBOOK_URL_SUFFIXES = {'bittrex': '', 'bitbay': '.json'}
+MARKETS_DICT_KEYS = {'bittrex': 'symbol', 'bitbay': 'items'}
+
 SEPARATORS = {'bittrex': '-', 'bitbay': ''}
-TYPE_URL_SUFFIXES = {'bittrex': '', 'bitbay': '.json'}
 
 
 class Offer:
@@ -28,7 +32,7 @@ def get_orders_from_api(site, market):
         headers = {'content-type': 'application/json'}
         currencies = market.split('-')
         response = requests.get(BASE_URLS[site] + currencies[0] + SEPARATORS[site] + currencies[1] + '/orderbook' +
-                                TYPE_URL_SUFFIXES[site], headers=headers)
+                                ORDERBOOK_URL_SUFFIXES[site], headers=headers)
 
         return response.json()
     except requests.exceptions.ConnectionError:
@@ -73,6 +77,40 @@ def get_data(site_list, markets):
     return offers
 
 
+def get_site_markets_json(site):
+    try:
+        headers = {'content-type': 'application/json'}
+        response = requests.get(GET_MARKETS_URLS[site], headers=headers)
+
+        return response.json()
+    except requests.exceptions.ConnectionError:
+        print("ERROR. API not available")
+
+    return None
+
+
+def convert_site_markets_to_list(markets_json, site):
+    if isinstance(markets_json, dict):
+        return [*markets_json[MARKETS_DICT_KEYS[site]].keys()]
+    else:
+        return [market[MARKETS_DICT_KEYS[site]] for market in markets_json]
+
+
+def get_markets_list(site):
+    markets_json = get_site_markets_json(site)
+    return convert_site_markets_to_list(markets_json, site)
+
+
+def find_common_markets(markets1, markets2):
+    return [market for market in markets1 if market in markets2]
+
+
+def get_common_markets_for_sites(site1, site2):
+    site1_markets = get_markets_list(site1)
+    site2_markets = get_markets_list(site2)
+    return find_common_markets(site1_markets, site2_markets)
+
+
 def print_list(ls):
     for item in ls:
         print(item)
@@ -83,8 +121,8 @@ def count_ratio(offer1, offer2):
 
 
 def count_profit(bid, bid_site, ask, ask_site):
-    quantity_in_deposit = ask.quantity * (1 - FEES[bid_site]['transfer'][bid.market.split('-')[0]]) * \
-                          (1 - FEES[ask_site]['transfer'][ask.market.split('-')[0]])
+    quantity_in_deposit = ask.quantity - FEES[bid_site]['transfer'][bid.market.split('-')[0]] \
+                          - FEES[ask_site]['transfer'][ask.market.split('-')[0]]
     quantity = quantity_in_deposit if quantity_in_deposit < bid.quantity else bid.quantity
     bid_value = quantity * bid.price * (1 - FEES[bid_site]['taker'])
     ask_value = quantity * ask.price * (1 - FEES[ask_site]['taker'])
@@ -109,8 +147,6 @@ def print_profit_info(bid, bid_site, ask, ask_site):
 
 
 def print_ratios(bid_offers, ask_offers, sites):
-    print_ratio_info(ask_offers[sites[0]], ask_offers[sites[1]], 'ask')
-    print_ratio_info(bid_offers[sites[0]], bid_offers[sites[1]], 'bid')
     print_ratio_info(bid_offers[sites[0]], ask_offers[sites[1]], 'arbitrage')
     print_profit_info(bid_offers[sites[0]], sites[0], ask_offers[sites[1]], sites[1])
     print_ratio_info(bid_offers[sites[1]], ask_offers[sites[0]], 'reverse arbitrage')
@@ -144,3 +180,4 @@ def get_datastream():
 
 if __name__ == '__main__':
     get_datastream()
+    print(get_common_markets_for_sites('bittrex', 'bitbay'))
