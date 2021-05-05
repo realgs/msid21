eee86082ef211c66e42dcf3c2ebf7aa7a7992090
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-05-05 22:16:24 +0200

    error checking

--- apiConnection.py
+++ apiConnection.py
@@ -1,18 +1,19 @@
 from aiohttp import ClientSession
+import json
 
 
 async def getApiResponse(path, successKey, successValue):
     headers = {'content-type': 'application/json'}
 
     async with ClientSession() as session:
-        async with session.get(path) as response:
-            response = await response.json()
-            if response[successKey] == successValue:
-                return response
+        async with session.get(path, headers=headers) as response:
+            if response.status == 200:
+                try:
+                    response = await response.json()
+                    if response[successKey] == successValue:
+                        return response
+                except json.decoder.JSONDecodeError:
+                    print("Incorrect api response format")
+            else:
+                print("Error while connecting to API.")
             return None
-
-    # except requests.exceptions.RequestException:
-    #     print("Error while connecting to API.")
-    # except json.decoder.JSONDecodeError:
-    #     print("Incorrect api response format")
-    # return None
