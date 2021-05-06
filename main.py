45ac2b2abcb543c72c5a4ed4afdeb69cdb54de09
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-05-06 14:43:25 +0200

    Adjusted profit calculation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from collections import deque
 
 from api.apis import APIS
 
+
 def findCommonMarkets(api1, api2):
     results = []
 
@@ -16,10 +17,10 @@ def findCommonMarkets(api1, api2):
 
 
 def calculateDifference(orderFrom, orderTo, apiFrom, apiTo, symbol):
-    quantity = min(orderFrom['quantity'], orderTo['quantity']) - apiFrom.withdrawalFee(symbol)
+    quantity = min(orderFrom['quantity'], orderTo['quantity']) * (1 - apiTo.transactionFee) - apiFrom.withdrawalFee(symbol)
 
     return {
-        'rate': (orderFrom['rate'] - orderTo['rate']) * (1 - apiTo.transactionFee),
+        'rate': (orderFrom['rate'] - orderTo['rate']),
         'quantity': quantity
     }
 
@@ -27,6 +28,7 @@ def calculateDifference(orderFrom, orderTo, apiFrom, apiTo, symbol):
 def calculateProfit(apiFrom, apiTo, symbol):
     queueFrom = deque(apiFrom.orderbook(symbol)['bid'])
     queueTo = deque(apiFrom.orderbook(symbol)['ask'])
+    currency = symbol.split('-')[0]
 
     previousProfit = 0
     profit = None
@@ -43,28 +45,31 @@ def calculateProfit(apiFrom, apiTo, symbol):
         orderFrom = queueFrom.pop()
         orderTo = queueTo.pop()
 
-        difference = calculateDifference(
-            orderFrom, orderTo, apiFrom, apiTo, symbol.split('-')[0])
+        difference = calculateDifference(orderFrom, orderTo, apiFrom, apiTo, currency)
+        
         if profit == None:
             previousProfit = profit = difference['rate']
-        else:
+            quantity = difference['quantity']
+            rateSum = orderFrom['rate']
+
+        if difference['quantity'] > 0:
             profit += difference['rate']
-        quantity += difference['quantity']
-        rateSum += orderFrom['rate']
+            quantity += difference['quantity']
+            rateSum += orderFrom['rate']
 
         # Putting back order with quantity left
-        if orderFrom['quantity'] > orderTo['quantity']:
+        if orderFrom['quantity'] > orderTo['quantity'] and orderFrom['quantity'] > apiFrom.withdrawalFee(currency):
             queueFrom.append({
                 'quantity': orderFrom['quantity'] - orderTo['quantity'],
                 'rate': orderFrom['rate']
             })
-        elif orderFrom['quantity'] < orderTo['quantity']:
+        elif orderFrom['quantity'] < orderTo['quantity'] and orderTo['quantity'] > apiFrom.withdrawalFee(currency):
             queueTo.append({
                 'quantity': orderTo['quantity'] - orderFrom['quantity'],
                 'rate': orderTo['rate']
             })
 
-    if rateSum != 0 and previousProfit < 0 and quantity < 0:
+    if rateSum != 0 and quantity < 0 and previousProfit < 0:
         return (previousProfit * quantity) / rateSum * -100
     elif rateSum != 0:
         return (previousProfit * quantity) / rateSum * 100
@@ -81,9 +86,12 @@ def main():
 
     # Ex2
     print("Ex2: ")
-    print(f"BTC-USD: {calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'BTC-USD'):.4f}%")
-    print(f"ETH-USD: {calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'ETH-USD'):.4f}%")
-    print(f"LTC-USD: {calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'LTC-USD'):.4f}%")
+    print(
+        f"BTC-USD:\t{calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'BTC-USD'):.4f}%")
+    print(
+        f"ETH-USD:\t{calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'ETH-USD'):.4f}%")
+    print(
+        f"LTC-USD:\t{calculateProfit(APIS['BITBAY'], APIS['BITTREX'], 'LTC-USD'):.4f}%")
 
     # Ex3
     print("Ex3: ")
@@ -94,7 +102,7 @@ def main():
     profits = sorted(profits, key=lambda el: float(el[1]), reverse=True)
 
     for profit in profits:
-        print(f"{profit[0]}:\t{profit[1]:.6f}%")
+        print(f"{profit[0]}:\t{profit[1]:.4f} %")
 
 
 if __name__ == "__main__":
