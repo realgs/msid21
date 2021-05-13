2ea7a0343492d40c7110cf101c34245e0eaae186
Jakub Ozog <carino9@tlen.pl>
2021-05-13 04:01:13 +0200

    Added listing markets functionality

--- L4project/BitbayLibrary.py
+++ L4project/BitbayLibrary.py
@@ -1,4 +1,5 @@
 import ApiRequest
+import re
 from BitbayData import BITBAY
 
 
@@ -25,5 +26,14 @@ def request_bids_and_asks(currencies: tuple[str, str]):
     else:
         raise Exception(f"Empty bids and asks list in BITBAY for ({currencies[0]},{currencies[1]})")
 
+def request_market_data():
+    markets = ApiRequest.make_request(f'{BITBAY["market_info_URL"]}')
+    markets_list = []
+    if markets is not None and markets["status"] == "Ok":
+        for market in markets["items"].keys():
+            symbols = re.split("-", market)
+            markets_list.append((symbols[0], symbols[1]))
+    return markets_list
 
 # print(request_bids_and_asks(("BTC", "USD")))
+# print(request_market_data())
--- L4project/BittrexLibrary.py
+++ L4project/BittrexLibrary.py
@@ -1,4 +1,5 @@
 import ApiRequest
+import re
 from BittrexData import BITTREX
 
 
@@ -10,5 +11,14 @@ def request_bids_and_asks(currencies: tuple[str, str]):
     else:
         raise Exception(f"Empty bids and asks list in BITTREX for ({currencies[0]},{currencies[1]})")
 
+def request_market_data():
+    markets = ApiRequest.make_request(f'{BITTREX["market_info_URL"]}')
+    markets_list = []
+    if markets is not None:
+        for market in markets:
+            symbols = re.split("-", market["symbol"])
+            markets_list.append((symbols[0], symbols[1]))
+    return markets_list
 
 # print(request_bids_and_asks(("BTC", "USD")))
+# print(request_market_data())
