fc214ad8f67c2a1bb393e2d8566315a28beef82b
jucic1 <julia.cicha60@gmail.com>
2021-05-19 16:50:55 +0200

    1,2,3 implemented

--- BittRexAPI.py
+++ BittRexAPI.py
@@ -3,15 +3,30 @@ import requests
 
 class BittRexAPI:
     BASEURL = 'https://api.bittrex.com/api/v1.1/public/'
-    VALID_CRYPTO_CURR = {"BTC", "LTC", "DASH"}
-    VALID_BASE_CURR = {"USD", "EUR", "CHF", "CAD", "AUD", "JPY", "GBP"}
+    MARKETS_BASEURL = 'https://api.bittrex.com/v3/'
+    VALID_CRYPTO_CURR = {'DOT', 'UNI', 'BSV', 'XRP', 'AAVE', 'XTZ', 'LTC', 'UNI', 'LINK', 'DOT', 'BSV', 'XTZ', 'GRT', 'AAVE', 'AAVE', 'PAY', 'ETH', 'ZRX', 'BSV', 'ZRX', 'TRX', 'BSV', 'XLM', 'OMG', 'BTC', 'BAT', 'LUNA', 'GRT', 'XLM', 'DAI', 'TRX', 'XRP', 'DAI', 'GAME', 'TRX', 'USDC', 'LTC', 'OMG', 'BTC', 'OMG', 'COMP', 'LSK', 'EOS', 'MANA', 'MKR', 'DOT', 'XRP', 'BSV', 'COMP', 'LTC', 'LSK', 'XLM', 'XRP', 'ETH', 'NPXS', 'UNI', 'TRX', 'ETH', 'BAT', 'LINK', 'TRX', 'GRT', 'XRP', 'XLM', 'EOS', 'LTC', 'ETH', 'BTC', 'SRN', 'XLM', 'LUNA', 'LINK'}
+    VALID_BASE_CURR = {'BTC', 'ETH', 'USDT', 'BTC', 'USD', 'BTC', 'BTC', 'EUR', 'BTC', 'USDT', 'BTC', 'BTC', 'ETH', 'EUR', 'USDT', 'USD', 'USD', 'BTC', 'BTC', 'BTC', 'BTC', 'EUR', 'BTC', 'ETH', 'USDT', 'BTC', 'EUR', 'USD', 'BTC', 'USDT', 'ETH', 'BTC', 'USDT', 'USDT', 'BTC', 'USDT', 'USDT', 'USDT', 'USDT', 'BTC', 'USD', 'ETH', 'USDT', 'USDT', 'BTC', 'BTC', 'EUR', 'ETH', 'USD', 'USDT', 'BTC', 'BTC', 'USDT', 'EUR', 'EUR', 'USD', 'USDT', 'USDT', 'USD', 'EUR', 'EUR', 'EUR', 'BTC', 'BTC', 'BTC', 'BTC', 'ETH', 'USD', 'USD', 'USD', 'USDT', 'BTC'}
     VALID_TYPE = {"buy", "sell", "both"}
     TAKER_FEE = 0.0035  # percentage
     TRANSFER_FEE = {
-        "BTC": 0.0005,
-        "LTC": 0.01,
-        "DASH": 0.05
+        'AAVE': 0.4, 'BAT': 35, 'BSV': 0.001, 'BTC': 0.0005, 'COMP': 0.05, 'DAI': 42, 'DOT': 0.5, 'EOS': 0.1,
+        'ETH': 0.006, 'EUR': 0, 'GAME': 133, 'GRT': 0, 'LINK': 1.15, 'LSK': 0.1, 'LTC': 0.01, 'LUNA': 2.2, 'MANA': 29,
+        'MKR': 0.0095, 'NPXS': 10967, 'OMG': 6, 'PAY': 351, 'SRN': 1567, 'TRX': 0.003, 'UNI': 1, 'USD': 0, 'USDC': 42,
+        'USDT': 42, 'XLM': 0.05, 'XRP': 1, 'XTZ': 0.25, 'ZRX': 25
     }
+
+    def get_markets_data(self):
+        query = self.MARKETS_BASEURL + "markets"
+        response = self.__query(query)
+        if response.status_code == 200:
+            return response.json()
+
+    def get_market_names_list(self):
+        all_markets = self.get_markets_data()
+        all_names = []
+        for market in all_markets:
+            all_names.append(market.get('symbol'))
+        return all_names
 
     def get_orderbook(self, crypto, base_curr, type):
         crypto = crypto.upper()
@@ -30,7 +45,7 @@ class BittRexAPI:
         if response.status_code == 200:
             print("INFO: Querying the API was successful.")
         else:
-            print("ERROR: API returned status code " + response.status_code)
+            print("ERROR: API returned status code " + str(response.status_code))
         return response
 
     def __is_valid(self, crypto, base_curr, type):
--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import random
+
 import time
 
 from BitBayAPI import BitBayAPI
@@ -8,70 +10,84 @@ BASE_CURRENCY = "USD"
 SLEEP_TIME = 5
 
 
-def exc_1(bitt_rex, bit_bay, crypto_currency, base_currency, abc):
-    print("EXC 1")
-    if abc == 'a':
-        print("Difference for buying " + crypto_currency + base_currency + " on bitbay and bittrex: ")
-        while True:
-            print(get_percentage_diff_for_best_buy_sell_on_diff_stock_ex(bitt_rex, bit_bay, crypto_currency,
-                                                                         base_currency,
-                                                                         "buy"))
-            time.sleep(SLEEP_TIME)
-    elif abc == "b":
-        print("Difference for selling " + crypto_currency + base_currency + " on bitbay and bittrex: ")
-        while True:
-            get_percentage_diff_for_best_buy_sell_on_diff_stock_ex(bitt_rex, bit_bay, crypto_currency, base_currency,
-                                                                   "sell")
-            time.sleep(SLEEP_TIME)
-    elif abc == "c":
-        print("Difference for buying " + crypto_currency + base_currency + " on bittrex and selling on bitbay: ")
-        while True:
-            get_percentage_diff_for_best_buy_sell_on_diff_stock_ex(bitt_rex, bit_bay, crypto_currency, base_currency,
-                                                                   "both")
-            time.sleep(SLEEP_TIME)
-    else:
-        print(abc + " not in scope : {a,b,b}")
+# W sposób automatyczny znaleźć wspólny zbiór par walutowych oferowanych przez dwie analizowane giełdy.
+def find_common_markets(bitt_rex, bit_bay):
+    all_markets_bitt_rex = bitt_rex.get_market_names_list()
+    all_markets_bit_bay = bit_bay.get_market_names_list()
+    common_markets = get_common_elements_in_two_lists(all_markets_bitt_rex, all_markets_bit_bay)
+    return common_markets
 
 
-def exc_2(bitt_rex, bit_bay, crypto_currency, base_currency):
-    print("EXC 2")
-    print("Arbitrage while buying " + crypto_currency + base_currency + " on bittrex and selling on bitbay: ")
+def get_common_elements_in_two_lists(list1, list2):
+    list1_as_set = set(list1)
+    return list(list1_as_set.intersection(list2))
+
+
+# Wykorzystując rozwiązanie szukające możliwości arbitrażu z listy poprzedniej (z uwzględnieniem opłat i wolumenów) sprawdzić możliwość wykonania arbitrażu na dowolnych trzech zasobach ze zbioru uzyskanego w zadaniu 1.
+# Upewnić się, że Wasze rozwiązanie szuka wgłąb rynku, a nie jedynie wśród pierwszych (najlepszych) ofert. Dla każdej pary walutowej zwrócić informację o potencjalnej wielkości zysku lub jego braku.
+def get_arbitrage_on_three_resources(bitt_rex, bit_bay):
+    common_markets = find_common_markets(bitt_rex, bit_bay)
+    three_common_markets = []
+    while len(three_common_markets) <= 3:
+        random_index = random.randint(0, len(common_markets) - 1)
+        three_common_markets.append(common_markets[random_index])
+
     while True:
-        print(calculate_arbitrage(bitt_rex, bit_bay, crypto_currency, base_currency))
+        for market in three_common_markets:
+            currency_pair = market.split("-")
+            print("Arbitrage while buying " + market + " on bittrex and selling on bitbay: ")
+            print(calculate_arbitrage(bitt_rex, bit_bay, currency_pair[0], currency_pair[1]))
         time.sleep(SLEEP_TIME)
+
+
+def rank_arbitrage_for_all_markets(bitt_rex, bit_bay):
+    common_markets = find_common_markets(bitt_rex, bit_bay)
+    arbitrage_map = {}
+    for market in common_markets:
+        current_pair = market.split("-")
+        arbitrage = calculate_arbitrage(bitt_rex, bit_bay, current_pair[0], current_pair[1])
+        if arbitrage is not None:
+            arbitrage_map[market] = arbitrage
+
+    sorted_arbitrage_map = sorted(arbitrage_map.items(), key=lambda x: x[1], reverse=True)
+    print("Arbitrage rank:")
+    print(sorted_arbitrage_map)
 
 
 def calculate_arbitrage(bitt_rex, bit_bay, crypto_currency, base_currency):
     bitt_rex_order_book_json = bitt_rex.get_orderbook(crypto_currency, base_currency, "sell")
-    bit_bay_order_book_json = bit_bay.get_transactions(crypto_currency, base_currency, "orderbook")
+    bit_bay_order_book_json = bit_bay.get_orderbook(crypto_currency, base_currency, "buy")
     bitt_rex_sell = bitt_rex_order_book_json['result']
-    quick_sort_bitt_rex_by_rate(bitt_rex_sell)
-    bitt_rex_best_sell = bitt_rex_sell[0]['Rate']
-    bitt_rex_best_sell_amount = bitt_rex_sell[0]['Quantity']
-    bit_bay_buy = bit_bay_order_book_json['bids']
-    quick_sort_bit_bay_by_rate(bit_bay_buy)
-    bit_bay_best_buy = bit_bay_buy[0][0]
-    bit_bay_best_buy_amount = bit_bay_buy[0][1]
-
-    if bit_bay_best_buy_amount < bitt_rex_best_sell_amount:  # if someone wants to buy less than i have to sell
-        fees_for_bitt_rex = get_fees(bitt_rex, bitt_rex_best_sell, bit_bay_best_buy_amount, crypto_currency)
-        fees_for_bit_bay = get_fees(bit_bay, bit_bay_best_buy, bit_bay_best_buy_amount, crypto_currency)
-        return bit_bay_best_buy * bit_bay_best_buy_amount - bitt_rex_best_sell * bit_bay_best_buy_amount - fees_for_bitt_rex - fees_for_bit_bay
+    bit_bay_buy = bit_bay_order_book_json
+    if len(bitt_rex_sell) == 0 or len(bit_bay_buy) == 0:
+        print("No offer was found for " + crypto_currency + "-" + base_currency + " on ", "bit_rex" if len(bitt_rex_sell) == 0 else "bit_bay")
     else:
-        missing_amount = bit_bay_best_buy_amount
-        total = 0
-        i = 0
+        quick_sort_by_rate(bitt_rex_sell, 'Rate')
+        bitt_rex_best_sell = bitt_rex_sell[0]['Rate']
+        bitt_rex_best_sell_amount = bitt_rex_sell[0]['Quantity']
+        quick_sort_by_rate(bit_bay_buy, 'ra')
+        bit_bay_best_buy = float(bit_bay_buy[0]['ra'])
+        bit_bay_best_buy_amount = float(bit_bay_buy[0]['ca'])
 
-        while missing_amount >= 0:
-            fees_for_bitt_rex = get_fees(bitt_rex, bitt_rex_best_sell, bitt_rex_best_sell_amount, crypto_currency)
-            fees_for_bit_bay = get_fees(bit_bay, bit_bay_best_buy, bitt_rex_best_sell_amount, crypto_currency)
-            total = total + bit_bay_best_buy * bit_bay_best_buy_amount - bitt_rex_best_sell * bit_bay_best_buy_amount - fees_for_bitt_rex - fees_for_bit_bay
-            missing_amount = missing_amount - bitt_rex_best_sell_amount
-            i = i + 1
-            bitt_rex_best_sell = bitt_rex_sell[i]['Rate']
-            bitt_rex_best_sell_amount = bitt_rex_sell[i]['Quantity']
+        if bit_bay_best_buy_amount < bitt_rex_best_sell_amount:  # if someone wants to buy less than i have to sell
+            fees_for_bitt_rex = get_fees(bitt_rex, bitt_rex_best_sell, bit_bay_best_buy_amount, crypto_currency)
+            fees_for_bit_bay = get_fees(bit_bay, bit_bay_best_buy, bit_bay_best_buy_amount, crypto_currency)
+            return bit_bay_best_buy * bit_bay_best_buy_amount - bitt_rex_best_sell * bit_bay_best_buy_amount - fees_for_bitt_rex - fees_for_bit_bay
+        else:
+            missing_amount = bit_bay_best_buy_amount
+            total = 0
+            i = 0
 
-        return total
+            while missing_amount >= 0:
+                fees_for_bitt_rex = get_fees(bitt_rex, bitt_rex_best_sell, bitt_rex_best_sell_amount, crypto_currency)
+                fees_for_bit_bay = get_fees(bit_bay, bit_bay_best_buy, bitt_rex_best_sell_amount, crypto_currency)
+                total = total + bit_bay_best_buy * bit_bay_best_buy_amount - bitt_rex_best_sell * bit_bay_best_buy_amount - fees_for_bitt_rex - fees_for_bit_bay
+                missing_amount = missing_amount - bitt_rex_best_sell_amount
+                i = i + 1
+                bitt_rex_best_sell = bitt_rex_sell[i]['Rate']
+                bitt_rex_best_sell_amount = bitt_rex_sell[i]['Quantity']
+
+            return total
 
 
 def get_fees(market, price, amount, crypto_currency):
@@ -81,38 +97,7 @@ def get_fees(market, price, amount, crypto_currency):
         raise Exception("Transfer fee not mapped for " + crypto_currency)
 
 
-def get_percentage_diff_for_best_buy_sell_on_diff_stock_ex(bitt_rex, bit_bay, crypto_currency, base_currency, type):
-    if type == "buy" or type == "sell" or type == "both":
-        bitt_rex_order_book_json = bitt_rex.get_orderbook(crypto_currency, base_currency, type)
-        bit_bay_order_book_json = bit_bay.get_transactions(crypto_currency, base_currency, "orderbook")
-        if type == "buy" or type == "sell":
-            bitt_rex_buy_sell = bitt_rex_order_book_json['result']
-            quick_sort_bitt_rex_by_rate(bitt_rex_buy_sell)
-            if type == "buy":
-                bit_bay_buy = bit_bay_order_book_json['bids']
-                quick_sort_bit_bay_by_rate(bit_bay_buy)
-                bit_bay_best_buy = bit_bay_buy[len(bit_bay_buy) - 1][0]
-                bitt_rex_best_sell = bitt_rex_buy_sell[len(bitt_rex_buy_sell) - 1]['Rate']
-                return bit_bay_best_buy / bitt_rex_best_sell - 1
-            else:
-                bit_bay_buy = bit_bay_order_book_json['asks']
-                quick_sort_bit_bay_by_rate(bit_bay_buy)
-                bit_bay_best_sell = bit_bay_buy[0][0]
-                bitt_rex_best_sell = bitt_rex_buy_sell[0]['Rate']
-                return bit_bay_best_sell / bitt_rex_best_sell - 1
-        else:
-            bitt_rex_sell = bitt_rex_order_book_json['result']['sell']
-            quick_sort_bitt_rex_by_rate(bitt_rex_sell)
-            bitt_rex_best_sell = bitt_rex_sell[0]['Rate']
-            bit_bay_buy = bit_bay_order_book_json['bids']
-            quick_sort_bit_bay_by_rate(bit_bay_buy)
-            bit_bay_best_buy = bit_bay_buy[0][0]
-            return bit_bay_best_buy / bitt_rex_best_sell - 1
-    else:
-        print("Wrong type provided. Type has to be either buy, sell or both. Not: " + type)
-
-
-def quick_sort_bit_bay_by_rate(unsorted):
+def quick_sort_by_rate(unsorted, rate_key):
     if len(unsorted) <= 1:
         return unsorted
 
@@ -122,33 +107,17 @@ def quick_sort_bit_bay_by_rate(unsorted):
     greater = []
 
     for item in unsorted:
-        if item[0] < pivot[0]:
+        if item.get(rate_key) < pivot.get(rate_key):
             lower.append(item)
         else:
             greater.append(item)
 
-    return quick_sort_bit_bay_by_rate(lower) + [pivot] + quick_sort_bit_bay_by_rate(greater)
-
-
-def quick_sort_bitt_rex_by_rate(unsorted):
-    if len(unsorted) <= 1:
-        return unsorted
-
-    pivot = unsorted.pop()
-
-    lower = []
-    greater = []
-
-    for item in unsorted:
-        if item['Rate'] < pivot['Rate']:
-            lower.append(item)
-        else:
-            greater.append(item)
-
-    return quick_sort_bitt_rex_by_rate(lower) + [pivot] + quick_sort_bitt_rex_by_rate(greater)
+    return quick_sort_by_rate(lower, rate_key) + [pivot] + quick_sort_by_rate(greater, rate_key)
 
 
 bitt_rex = BittRexAPI()
 bit_bay = BitBayAPI()
-# exc_1(bitt_rex, bit_bay, CRYPTO_CURRENCY, BASE_CURRENCY, "a")
-exc_2(bitt_rex, bit_bay, CRYPTO_CURRENCY, BASE_CURRENCY)
+# find_common_markets(bitt_rex, bit_bay)
+# get_arbitrage_on_three_resources(bitt_rex, bit_bay)
+rank_arbitrage_for_all_markets(bitt_rex, bit_bay)
+# calculate_arbitrage(bitt_rex, bit_bay, "NPXS", "BTC")
