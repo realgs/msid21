1ede4a214db1e16ee22446fcd9bd360e23b1cf0e
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-05-20 11:36:53 +0200

    Improved NBP api

--- api/nbp.py
+++ api/nbp.py
@@ -5,18 +5,24 @@ class NBP(Api):
     def __init__(self):
         super().__init__("NBP", "http://api.nbp.pl/api/exchangerates/rates")
         self.__tables = ['A', 'B', 'C']
+        self.__fetched = {}
 
     def orderbook(self, symbol):
         return None
 
     def ticker(self, symbol):
         currency = symbol.split('-')[0]
-        for table in self.__tables:
-            url = f"{self._url}/{table}/{currency}/"
-            try:
-                response = self.request(url)
-                return {'price': float(response['rates'][0]['mid'])}
-            except ConnectionError:
-                pass
+        if currency in self.__fetched:
+            return {'price': self.__fetched[currency]}
+        else:
+            for table in self.__tables:
+                url = f"{self._url}/{table}/{currency}/"
+                try:
+                    response = self.request(url)
+                    price = float(response['rates'][0]['mid'])
+                    self.__fetched[currency] = price
+                    return {'price': price}
+                except ConnectionError:
+                    pass
 
         raise ValueError(f'Wrong currency: {currency}')
