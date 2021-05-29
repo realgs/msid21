31425a2f3bd1a04e35dbac414583bf820666d011
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-05-29 20:50:32 +0200

    configuration dir

--- services/configurationService.py
+++ services/configurationService.py
@@ -1,10 +1,12 @@
 import os
 import json
 
+DIRECTORY = 'configurations/'
+
 
 def readConfig(fileName):
-    if os.path.isfile(fileName):
-        with open(fileName) as json_file:
+    if os.path.isfile(DIRECTORY + fileName):
+        with open(DIRECTORY + fileName) as json_file:
             data = json.load(json_file)
             return data
     else:
@@ -13,7 +15,9 @@ def readConfig(fileName):
 
 def saveConfig(fileName, data):
     try:
-        with open(fileName, 'w') as outfile:
+        if not os.path.exists(DIRECTORY):
+            os.makedirs(DIRECTORY)
+        with open(DIRECTORY + fileName, 'w') as outfile:
             json.dump(data, outfile)
         return True
     except OSError:
