ece0a30a9d86c320d38076273d19be5a2abe65ca
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-05-31 17:11:16 +0200

    filter data

--- api/twelveData.py
+++ api/twelveData.py
@@ -3,7 +3,7 @@ from services.connectionService import getApiResponse
 import credentials
 
 NAME = "TwelveData"
-TAKER_FEE = 0  # TODO: Set fees
+TAKER_FEE = 0  # Could not found information
 DEFAULT_TRANSFER_FEE = 0
 API_BASE_URL = "https://twelve-data1.p.rapidapi.com/"
 BASE_VALUE = "USD"
@@ -16,28 +16,35 @@ class TwelveData(Api):
     def __init__(self, cantorService):
         super().__init__(NAME, TAKER_FEE)
         self.cantorService = cantorService
-        self.headers = {'x-rapidapi-key': credentials.X_RAPIDAPI_KEY, 'x-rapidapi-host': X_RAPIDAPI_HOST}
+        self._headers = {'x-rapidapi-key': credentials.X_RAPIDAPI_KEY, 'x-rapidapi-host': X_RAPIDAPI_HOST}
+        self._availableMarkets = set()
+
+    @property
+    async def availableMarkets(self):
+        if not self._availableMarkets:
+            apiResult = await getApiResponse(f"{API_BASE_URL}stocks?exchange=NASDAQ&format=json", headers=self._headers)
+            if STATUS_KEY not in apiResult or apiResult[STATUS_KEY] != STATUS_ERR:
+                self._availableMarkets = [marketData['symbol'] for marketData in apiResult['data']]
+        return self._availableMarkets
 
     async def getTransferFee(self, resource):
         return DEFAULT_TRANSFER_FEE
 
     async def getBestOrders(self, resources, amount=None):
         symbol, currency = resources
-        apiResult = await getApiResponse(f"{API_BASE_URL}price?symbol={symbol}&format=json&outputsize=30",
-                                         headers=self.headers)
-        if apiResult is None:
-            return {"success": False, "cause": f"To many calls for api {self.getName()}"}
-        if apiResult is not None and STATUS_KEY not in apiResult or apiResult[STATUS_KEY] != STATUS_ERR:
+        if symbol not in await self.availableMarkets:
+            return {"success": False, "cause": f"Cannot retrieve data fot resource: {symbol}"}
+
+        apiResult = await getApiResponse(f"{API_BASE_URL}price?symbol={symbol}&format=json&outputsize=30", headers=self._headers)
+        if apiResult is not None and (STATUS_KEY not in apiResult or apiResult[STATUS_KEY] != STATUS_ERR):
             price = await self.cantorService.convertCurrencies(BASE_VALUE, currency, float(apiResult['price']))
             value = [{"price": price, 'quantity': 0}]
             return {"success": True, "buys": value, "sells": value}
-        return {"success": False, "cause": f"Cannot retrieve data fot resource: {symbol} or numer of calls exceeded"}
+        return {"success": False, "cause": f"Cannot retrieve data fot resource: {symbol} or to many calls for api {self.getName()}"}
 
     async def getAvailableMarkets(self):
-        apiResult = await getApiResponse(f"{API_BASE_URL}stocks?exchange=NASDAQ&format=json", headers=self.headers)
-        if STATUS_KEY not in apiResult or apiResult[STATUS_KEY] != STATUS_ERR:
-            markets = []
-            for marketData in apiResult['data']:
-                markets.append({'currency1': marketData['symbol'], 'currency2': BASE_VALUE})
+        data = await self.availableMarkets
+        if data:
+            markets = [{'currency1': market, 'currency2': BASE_VALUE} for market in data]
             return {"success": True, 'markets': markets}
         return {"success": False, "cause": "Cannot retrieve data"}
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import asyncio
+from services.cantorService import NBPCantorService
+from api.twelveData import TwelveData
+from api.bitbay import Bitbay
 
 
 async def main():
-    pass
+    x = TwelveData(NBPCantorService())
+    y = Bitbay()
+    print(await x.getBestOrders(('AAL', 'EUR')))
+    print(await x.getBestOrders(('TECH', 'PLN')))
+    print(await x.getBestOrders(('BTC', 'PLN')))
 
 
 loop = asyncio.get_event_loop()
