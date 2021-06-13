28a8052a6396cc31ffe456f5055777e161cb510c
bedni <w.bednarz2000@gmail.com>
2021-06-13 22:07:40 +0200

    Task 1 implementation

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,5 +1,33 @@
+import json
+
+CONFIG_FILE = "config.json"
+
+
+def get_resources():
+    base_currency, resources = None, None
+
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            data = json.load(config_file)
+
+        base_currency = data['base_currency']
+        resources = data['resources']
+
+    except FileNotFoundError:
+        print("File '{}' not found.".format(CONFIG_FILE))
+    except KeyError as ke:
+        print("File error - {} not found. Please reformat file.".format(ke))
+
+    return base_currency, resources
+
+
 def main():
-    pass
+    base_currency, resources = get_resources()
+
+    if not base_currency or not resources:
+        exit()
+
+    print("base_currency: {}\nresources: {}".format(base_currency, resources))
 
 
 if __name__ == '__main__':
