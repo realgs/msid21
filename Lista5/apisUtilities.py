4464d28f3f3a71650f1173a205361df41c68eafd
marcijay <jak.marcinko@gmail.com>
2021-06-14 23:11:06 +0200

    Implemented tools needed

--- Lista5/apisUtilities.py
+++ Lista5/apisUtilities.py
@@ -1,8 +1,11 @@
 import time
-import yfinance as yf
 import requests
+import yfinance as yf
+from bs4 import BeautifulSoup
+from common import load_data_from_json
 
-import common
+NORMALIZED_OPERATIONS = ['bids', 'asks']
+WAITING_TIME = 1
 
 
 def get_api_response(url):
@@ -14,60 +17,18 @@ def get_api_response(url):
         return None
 
 
-def get_bitrex_fees(apiInfo):
-    fees = get_api_response(apiInfo['API']['bitrex']['url_currencies'])
-    dictionary = {}
-    if fees.get("result", None):
-        items = fees['result']
-        for entry in items:
-            if entry.get('Currency', None) and entry.get('TxFee', None):
-                dictionary[entry['Currency']] = entry["TxFee"]
-
-    return dictionary
-
-
-def get_transfer_fees(apiInfo, pairs):
-    fees = {"bitbay": {}, "bitrex": {}}
-    bitrexFees = get_bitrex_fees(apiInfo)
+def get_transfer_fees(stockFst, stockSnd, pairs):
+    fees = {stockFst.get_name(): {}, stockSnd.get_name(): {}}
+    fstStockFees = stockFst.get_withdrawal_fees()
+    sndStockFees = stockSnd.get_withdrawal_fees()
     for pair in pairs:
-        fees['bitbay'][pair[0]] = apiInfo['FEES']["bitbay_fees"][pair[0]]
-        fees['bitrex'][pair[0]] = bitrexFees[pair[0]]
+        fees[stockFst.get_name()][pair[0]] = fstStockFees[pair[0]]
+        fees[stockSnd.get_name()][pair[0]] = sndStockFees[pair[0]]
     return fees
 
 
-def parse_bitbay_currencies(jsonData):
-    result = []
-    if jsonData.get("items", None):
-        items = jsonData['items']
-        for entry in items.keys():
-            pair = entry.split('-')
-            result.append((pair[0], pair[1]))
-    return result
-
-
-def parse_bitrex_currencies(jsonData):
-    result = []
-    if jsonData.get("result", None):
-        for entry in jsonData['result']:
-            if entry.get("MarketCurrency", None) and entry.get("BaseCurrency", None):
-                result.append((entry['MarketCurrency'], entry['BaseCurrency']))
-    return result
-
-
-def get_currency_pairs(apiInfo, market):
-    marketInfo = get_api_response(apiInfo['API'][market]['url_markets'])
-    if marketInfo is not None:
-        if market == apiInfo['API']['bitbay']['name']:
-            return parse_bitbay_currencies(marketInfo)
-        elif market == apiInfo['API']['bitrex']['name']:
-            return parse_bitrex_currencies(marketInfo)
-
-
-def find_common_currencies_pairs(apiInfo):
-    bitbayPairs = get_currency_pairs(apiInfo, apiInfo['API']['bitbay']['name'])
-    bitrexPairs = get_currency_pairs(apiInfo, apiInfo['API']['bitrex']['name'])
-
-    return set(bitrexPairs).intersection(bitbayPairs)
+def find_common_currencies_pairs(fstStockPairs, sndStockPairs):
+    return set(fstStockPairs).intersection(sndStockPairs)
 
 
 def exchange_base_currency(sourceCurrency, targetCurrency, quantity, apiInfo):
@@ -86,80 +47,61 @@ def exchange_base_currency(sourceCurrency, targetCurrency, quantity, apiInfo):
     return exchange_base_currency('PLN', targetCurrency, plnValue, apiInfo)
 
 
+def get_current_foreign_stock_price(stock, baseCurrency, apiInfo):
+    try:
+        tickerInfo = yf.Ticker(stock).info
+        currency = tickerInfo['currency'] if tickerInfo['currency'] else "USD"
+        return exchange_base_currency(currency, baseCurrency,
+                                      (float(tickerInfo['dayLow']) + float(tickerInfo['dayHigh'])) / 2, apiInfo)
+    except Exception:
+        raise ValueError(f'Wrong symbol: {stock}')
+
+
+def get_current_pl_stock_price(stock, baseCurrency, apiInfo):
+    stooqUrl = apiInfo["API"]["stooq"]["url_stock"]
+    try:
+        response = requests.get(f"{stooqUrl}{stock.lower()}").text
+        soup = BeautifulSoup(response, 'html.parser')
+        price = float(soup.find(id="t1").find('td').find('span').get_text())
+        return exchange_base_currency("PLN", baseCurrency, price, apiInfo)
+
+    except Exception:
+        raise ValueError(f'Wrong symbol: {stock}')
+
+
 if __name__ == '__main__':
-    data = common.load_data_from_json("data.json")
-    apisInfo = common.load_data_from_json("apis.json")
-    print(round(exchange_base_currency(data['currencies'][0]["symbol"], data["baseCurrency"], data['currencies'][0]["quantity"], apisInfo), 2))
-    print(yf.Ticker("AAPL").info)
+    data = load_data_from_json("data.json")
+    apisInfo = load_data_from_json("apis.json")
+    print(exchange_base_currency(data['currencies'][0]["symbol"], data["baseCurrency"],
+                                 data['currencies'][0]["quantity"], apisInfo))
 
+    print(get_current_foreign_stock_price(data['foreignStock'][0]["symbol"], data['baseCurrency'], apisInfo))
 
-NORMALIZED_OPERATIONS = ['bids', 'asks']
-WAITING_TIME = 3
-
-
-def parse_bitrex_orderbook(jsonData):
-    resultDictionary = {}
-    if jsonData.get("result", None):
-        table = []
-        if jsonData["result"].get("buy", None):
-            pair = []
-            for dictionary in jsonData["result"]["buy"]:
-                pair.append(dictionary["Rate"])
-                pair.append(dictionary["Quantity"])
-                table.append(pair.copy())
-                pair.clear()
-            resultDictionary[NORMALIZED_OPERATIONS[0]] = table.copy()
-            table.clear()
-        if jsonData["result"].get("sell", None):
-            pair = []
-            for dictionary in jsonData["result"]["sell"]:
-                pair.append(dictionary["Rate"])
-                pair.append(dictionary["Quantity"])
-                table.append(pair.copy())
-                pair.clear()
-            resultDictionary[NORMALIZED_OPERATIONS[1]] = table.copy()
-            table.clear()
-    return resultDictionary
-
-
-def get_offers(currency, market, baseCurrency, apiInfo):
-    if market == apiInfo["API"]["bitbay"]["name"]:
-        offers = get_api_response(f'{apiInfo["API"]["bitbay"]["url_orderbook"]}{currency}'
-                                  f'{baseCurrency}{apiInfo["API"]["bitbay"]["orderbook_ending"]}')
-        if offers is not None:
-            return offers
-
-    elif market == apiInfo["API"]["bitrex"]["name"]:
-        offers = get_api_response(f'{apiInfo["API"]["bitrex"]["url_orderbook"]}{baseCurrency}'
-                                  f'{apiInfo["API"]["bitrex"]["orderbook_separator"]}'
-                                  f'{currency}{apiInfo["API"]["bitrex"]["orderbook_ending"]}')
-        if offers is not None:
-            return parse_bitrex_orderbook(offers)
-
-    else:
-        return None
+    print(get_current_pl_stock_price(data["plStock"][0]['symbol'], data['baseCurrency'], apisInfo))
 
 
 def calculate_percentage_difference(order1, order2):
     return round(((order1 - order2) / order1) * 100, 2)
 
 
-def include_taker_fee(cost, market, operation):
+def include_taker_fee(cost, stock, operation):
     if operation == NORMALIZED_OPERATIONS[1]:
-        return cost * (1 + market["taker_fee"])
+        return cost * (1 + stock.get_taker_fee())
     elif operation == NORMALIZED_OPERATIONS[0]:
-        return cost * (1 - market["taker_fee"])
+        return cost * (1 - stock.get_taker_fee())
 
 
 def calculate_order_value(price, amount):
     return price * amount
 
 
-def calculate_arbitrage(offersToBuyFrom, offersToSellTo, buyingMarket, sellingMarket, currency, baseCurrency, fees):
+def calculate_arbitrage(sourceStock, targetStock, currency, baseCurrency, fees):
+    offersToBuyFrom = sourceStock.get_offers()[NORMALIZED_OPERATIONS[1]]
+    offersToSellTo = targetStock.get_offers()[NORMALIZED_OPERATIONS[0]]
     volume = 0.0
     spentMoney = 0.0
     gainedMoney = 0.0
-    transferFee = fees[buyingMarket['name']][currency]
+    transferFee = fees[sourceStock.get_name()][currency]
     transferFeePaidNumber = 0
     operationNumber = 0
 
@@ -176,9 +118,9 @@ def calculate_arbitrage(offersToBuyFrom, offersToSellTo, buyingMarket, sellingMa
                 transferFee -= buyVolume
                 sellVolume = 0
         buyValue = calculate_order_value(offersToBuyFrom[0][0], buyVolume)
-        buyCost = include_taker_fee(buyValue, buyingMarket, NORMALIZED_OPERATIONS[1])
+        buyCost = include_taker_fee(buyValue, sourceStock, NORMALIZED_OPERATIONS[1])
         sellValue = calculate_order_value(offersToSellTo[0][0], sellVolume)
-        sellGain = include_taker_fee(sellValue, sellingMarket, NORMALIZED_OPERATIONS[0])
+        sellGain = include_taker_fee(sellValue, targetStock, NORMALIZED_OPERATIONS[0])
 
         if buyCost < sellGain or transferFee > 0 or (transferFee == 0.0 and operationNumber == transferFeePaidNumber):
             volume += buyVolume
@@ -206,49 +148,42 @@ def calculate_arbitrage(offersToBuyFrom, offersToSellTo, buyingMarket, sellingMa
     else:
         profitability = round(((gainedMoney - spentMoney) / spentMoney) * 100, 2)
 
-    return {'volume': volume, 'profitability': profitability, 'profit': round(gainedMoney - spentMoney, 2)}
+    return {'volume': volume, 'profitability': profitability, 'profit': round(gainedMoney - spentMoney, 5)}
 
 
-def zad3(apiInfo, transferfees, currencypairs):
-    resultsFromBitbayToBitrex = []
-    resultsFromBitrexToBitbay = []
-    for currency in currencypairs:
+def zad6(fstStock, sndStock, transferFees, currencyPairs):
+    resultsFromFstToSnd = []
+    resultsFromSndToFst = []
+    for currency in currencyPairs:
         crypto = currency[0]
         base_currency = currency[1]
         print(f"Cryptocurrency: {crypto}, base currency: {base_currency}")
-        i = 0
-        offer1 = get_offers(crypto, apiInfo["API"]["bitbay"]["name"], base_currency, apiInfo)
-        offer2 = get_offers(crypto, apiInfo["API"]["bitrex"]["name"], base_currency, apiInfo)
+        offer1 = fstStock.get_offers()
+        offer2 = sndStock.get_offers()
         if offer1 is not None and offer2 is not None:
             if offer1.get(NORMALIZED_OPERATIONS[0], None) and offer1.get(NORMALIZED_OPERATIONS[1], None) \
                     and offer2.get(NORMALIZED_OPERATIONS[0], None) and offer2.get(NORMALIZED_OPERATIONS[1], None):
-                print(f'{apiInfo["API"]["bitbay"]["name"]} - selling offers: {offer1[NORMALIZED_OPERATIONS[1]]}\n'
-                      f'{apiInfo["API"]["bitbay"]["name"]} - buying offers: {offer1[NORMALIZED_OPERATIONS[0]]}')
-                print(f'{apiInfo["API"]["bitrex"]["name"]} - selling offer: {offer2[NORMALIZED_OPERATIONS[1]]}\n'
-                      f'{apiInfo["API"]["bitrex"]["name"]} - buying offers: {offer2[NORMALIZED_OPERATIONS[0]]}')
-                resultFrom1To2 = calculate_arbitrage(offer1[NORMALIZED_OPERATIONS[1]],
-                                                     offer2[NORMALIZED_OPERATIONS[0]],
-                                                     apiInfo["API"]["bitbay"], apiInfo["API"]["bitrex"], crypto,
-                                                     base_currency, transferfees)
-                resultsFromBitbayToBitrex.append((crypto, base_currency, resultFrom1To2))
+                print(f'{fstStock.get_name()} - selling offers: {offer1[NORMALIZED_OPERATIONS[1]]}\n'
+                      f'{fstStock.get_name()} - buying offers: {offer1[NORMALIZED_OPERATIONS[0]]}')
+                print(f'{sndStock.get_name()} - selling offer: {offer2[NORMALIZED_OPERATIONS[1]]}\n'
+                      f'{sndStock.get_name()} - buying offers: {offer2[NORMALIZED_OPERATIONS[0]]}')
+                resultFrom1To2 = calculate_arbitrage(fstStock, sndStock, crypto, base_currency, transferFees)
+                resultsFromFstToSnd.append((crypto, base_currency, resultFrom1To2))
                 if resultFrom1To2['profit'] <= 0:
-                    print(f"There are no profitable transactions buying in {apiInfo['API']['bitbay']['name']}"
-                          f" and selling in {apiInfo['API']['bitrex']['name']}")
+                    print(f"There are no profitable transactions buying in {fstStock.get_name()}"
+                          f" and selling in {sndStock.get_name()}")
                 else:
-                    print(f"Buying from {apiInfo['API']['bitbay']['name']}:\n "
+                    print(f"Buying from {fstStock.get_name()}:\n "
                           f"Volume: {resultFrom1To2['volume']} {crypto},"
                           f" profit: {resultFrom1To2['profit']} {base_currency},"
                           f" gain: {resultFrom1To2['profitability']}%")
-                resultFrom2To1 = calculate_arbitrage(offer2[NORMALIZED_OPERATIONS[1]],
-                                                     offer1[NORMALIZED_OPERATIONS[0]],
-                                                     apiInfo["API"]["bitrex"], apiInfo["API"]["bitbay"], crypto,
-                                                     base_currency, transferfees)
-                resultsFromBitrexToBitbay.append((crypto, base_currency, resultFrom2To1))
+                resultFrom2To1 = calculate_arbitrage(sndStock, fstStock, crypto, base_currency, transferFees)
+                resultsFromSndToFst.append((crypto, base_currency, resultFrom2To1))
                 if resultFrom2To1['profit'] <= 0:
-                    print(f"There are no profitable transactions buying in {apiInfo['API']['bitrex']['name']}"
-                          f" and selling in {apiInfo['API']['bitbay']['name']}")
+                    print(f"There are no profitable transactions buying in {sndStock.get_name()}"
+                          f" and selling in {fstStock.get_name()}")
                 else:
-                    print(f"Buying from {apiInfo['API']['bitrex']['name']}:\n "
+                    print(f"Buying from {sndStock.get_name()}:\n "
                           f"Volume: {resultFrom2To1['volume']} {crypto},"
                           f" profit: {resultFrom2To1['profit']} {base_currency},"
                           f" gain: {resultFrom2To1['profitability']}%")
@@ -257,6 +192,5 @@ def zad3(apiInfo, transferfees, currencypairs):
         else:
             print("Something gone wrong during data acquisition")
         time.sleep(WAITING_TIME)
-        i += 1
         print()
-    return resultsFromBitbayToBitrex, resultsFromBitrexToBitbay
+    return resultsFromFstToSnd, resultsFromSndToFst
---
+++ Lista5/cryptoApis/bitbay.py
@@ -0,0 +1,38 @@
+from common import load_data_from_json
+from apisUtilities import get_api_response
+
+
+def parse_bitbay_currencies(jsonData):
+    result = []
+    if jsonData.get("items", None):
+        items = jsonData['items']
+        for entry in items.keys():
+            pair = entry.split('-')
+            result.append((pair[0], pair[1]))
+    return result
+
+
+class Bitbay:
+    def __init__(self):
+        self.__data = load_data_from_json("..apis.json")['API']['bitbay']
+        self.__fees = load_data_from_json("..apis.json")['FEES']["bitbay_fees"]
+
+    def get_offers(self, currency, baseCurrency):
+        offers = get_api_response(f'{self.__data["url_orderbook"]}{currency}'
+                                  f'{baseCurrency}{self.__data["orderbook_ending"]}')
+        if offers is not None:
+            return offers
+
+    def get_taker_fee(self):
+        return self.__data['taker_fee']
+
+    def get_markets(self):
+        marketInfo = get_api_response(self.__data['url_markets'])
+        if marketInfo is not None:
+            return parse_bitbay_currencies(marketInfo)
+
+    def get_withdrawal_fees(self):
+        return self.__fees
+
+    def get_name(self):
+        return self.__data['name']
